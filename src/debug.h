d9ef439c9550a57f2192b69677f796d475495a37
cdfrey <cdfrey>
2005-11-11 23:52:17 +0000

    - added eout() and ddout() debugging - to separate:         - regular debug output         - exception debug output (should probably be logged to                 syslog someday, and never turned off)         - raw data dump output

--- src/debug.h
+++ src/debug.h
@@ -1,14 +1,32 @@
 
-#ifndef __DEBUG_H__
-#define __DEBUG_H__
+//#ifndef __DEBUG_H__
+//#define __DEBUG_H__
 
 #include <iostream>		// debugging only
 
 #ifdef __DEBUG_MODE__
-	#define dout(x)  std::cout << x << std::endl
-#else
+	#undef dout
+	#undef eout
+	#undef ddout
+
+	// debug output
+//	#define dout(x)  	std::cout << x << std::endl
 	#define dout(x)
+
+	// exception output
+	#define eout(x)  	std::cout << x << std::endl
+
+	// data dump output
+	#define ddout(x)	std::cout << x << std::endl
+#else
+	#undef dout
+	#undef eout
+	#undef ddout
+
+	#define dout(x)
+	#define eout(x)  	std::cout << x << std::endl
+	#define ddout(x)
 #endif
 
-#endif
+//#endif
 
