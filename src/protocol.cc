17c75244e99e4b71563bf24da7cf11f1c127ea90
cdfrey <cdfrey>
2005-11-11 23:54:40 +0000

    - added common packet size checker in protocol.cc - fixed unpacked protocol packet structs... using gcc's         __attribute__ ((packed)) ... see protocol.h - added ModeSelectCommand structure (protocol.h)

--- src/protocol.cc
+++ src/protocol.cc
@@ -4,4 +4,29 @@
 ///
 
 #include "protocol.h"
+#include "data.h"
+#include "error.h"
+#include "debug.h"
+
+#include <sstream>
+
+namespace Syncberry {
+
+void CheckSize(const Data &packet, int requiredsize)
+{
+	const Packet *p = (const Packet *) packet.GetData();
+	if( p->size != (unsigned int) packet.GetSize() ||
+	    packet.GetSize() < requiredsize )
+	{
+		std::ostringstream oss;
+		oss << "Bad packets size. Packet: " << p->size
+		    << ". DataSize(): " << packet.GetSize()
+		    << ". Required size: " << requiredsize;
+		eout(oss.str());
+		eout(packet);
+		throw SBError(oss.str());
+	}
+}
+
+} // namespace Syncberry
 
--- src/protocol.h
+++ src/protocol.h
@@ -8,6 +8,9 @@
 
 #include <stdint.h>
 
+// forward declarations
+class Data;
+
 namespace Syncberry {
 
 
@@ -15,7 +18,7 @@ struct SocketCommand
 {
 	uint16_t	socket;
 	uint8_t		param;
-};
+} __attribute__ ((packed));
 
 struct SequenceCommand
 {
@@ -23,20 +26,27 @@ struct SequenceCommand
 	uint8_t		unknown2;
 	uint8_t		unknown3;
 	uint32_t	sequenceId;
-};
+} __attribute__ ((packed));
+
+struct ModeSelectCommand
+{
+	uint8_t		unknown1;
+	uint8_t		unknown2;	// starts at 0x05... should figure this
+	uint8_t		mode_name[16];
+} __attribute__ ((packed));
 
 struct DBCommand
 {
 	uint8_t		command;	// see below
 	uint16_t	database_id;	// value from the Database Database
 	uint8_t		data[1];
-};
+} __attribute__ ((packed));
 
 struct DBResponse
 {
 	uint8_t		command;
 	uint8_t		data[1];
-};
+} __attribute__ ((packed));
 
 struct Packet
 {
@@ -54,8 +64,8 @@ struct Packet
 				SocketCommand	socket;
 				SequenceCommand	sequence;
 				uint8_t		raw[1];
-			} data;
-		} simple;
+			} __attribute__ ((packed)) data;
+		}  __attribute__ ((packed)) simple;
 
 
 		// and some commands do, even in their fragmented packets
@@ -64,14 +74,15 @@ struct Packet
 			uint8_t		param;
 			union ParamPacketData
 			{
-				DBCommand	db;
-				DBResponse	db_r;
-				uint8_t		raw[1];
-			} data;
-		} param;
+				ModeSelectCommand	mode;
+				DBCommand		db;
+				DBResponse		db_r;
+				uint8_t			raw[1];
+			} __attribute__ ((packed)) data;
+		}  __attribute__ ((packed)) param;
 
-	} data;
-};
+	} __attribute__ ((packed)) data;
+} __attribute__ ((packed));
 
 // minimum required sizes for various responses
 #define MIN_PACKET_SIZE		6
@@ -86,6 +97,8 @@ struct Packet
 #define SB_PARAM_HEADER_SIZE	(sizeof(Syncberry::Packet::PacketData::ParamPacket) - sizeof(Syncberry::Packet::PacketData::ParamPacket::ParamPacketData))
 #define SB_FRAG_HEADER_SIZE	(SB_PACKET_HEADER_SIZE + SB_PARAM_HEADER_SIZE)
 #define SB_SEQUENCE_PACKET_SIZE	(SB_PACKET_HEADER_SIZE + sizeof(Syncberry::SequenceCommand))
+#define SB_SOCKET_PACKET_SIZE	(SB_PACKET_HEADER_SIZE + sizeof(Syncberry::SocketCommand))
+#define SB_MODE_PACKET_SIZE	(SB_PACKET_HEADER_SIZE + SB_PARAM_HEADER_SIZE + sizeof(Syncberry::ModeSelectCommand))
 
 
 
@@ -99,13 +112,19 @@ struct Packet
 
 
 // packet commands (Packet.command: has response codes too)
+#define SB_COMMAND_SELECT_MODE		0x07
 #define SB_COMMAND_OPEN_SOCKET		0x0a
 #define SB_COMMAND_CLOSE_SOCKET		0x0b
 #define SB_COMMAND_CLOSED_SOCKET	0x0c	// response #2
+#define SB_COMMAND_OPENED_SOCKET	0x10
 #define SB_COMMAND_SEQUENCE_HANDSHAKE	0x13
 #define SB_COMMAND_DB_DATA		0x40
 #define SB_COMMAND_DB_FRAGMENTED	0x60
 #define SB_COMMAND_DB_DONE		0x41
+
+
+// param command parameters
+#define SB_PARAM_DEFAULT		0xff
 
 
 // DB Operation Command
@@ -113,6 +132,11 @@ struct Packet
 #define SB_DBOP_GET_COUNT		0x4e
 #define SB_DBOP_GET_RECORDS		0x4f
 
+
+// Macros
+#define COMMAND(data)			(((const Syncberry::Packet *)data.GetData())->command)
+#define IS_COMMAND(data, cmd)		(COMMAND(data) == cmd)
+#define MAKE_PACKET(var, data)		const Syncberry::Packet *var = (const Syncberry::Packet *) data.GetData()
 
 // fragmentation protocol
 // send DATA first, then keep sending DATA packets, FRAGMENTing
@@ -122,6 +146,11 @@ struct Packet
 //
 // the fragmented piece only has a the param header, and then continues
 // right on with the data
+
+
+
+// checks packet size and throws SBError if not right
+void CheckSize(const Data &packet, int requiredsize = MIN_PACKET_SIZE);
 
 } // namespace Syncberry
 
