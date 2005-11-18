65ede915422ceb62dbefa88a30b841794aa209d6
cdfrey <cdfrey>
2005-11-18 03:26:32 +0000

    - added mode selection support to Blackberry class (RIM Desktop,         RIM Bypass, and RIM_JavaLoader)         Mode selection governs which socket is used in subsequent         data transfers. - added Command Table parsing and protocol support         Blackberry class, and CommandTable record class - added Database database parsing and protocol support         Blackberry class and DatabaseDatabase record class - added Address book retrieval support - dumps data to stdout for now

--- src/blackberry.cc
+++ src/blackberry.cc
@@ -8,7 +8,13 @@
 #include "protocol.h"
 #include "error.h"
 #include "data.h"
+
+#define __DEBUG_MODE__
 #include "debug.h"
+
+#include <sstream>
+
+#include <iomanip>
 
 namespace Syncberry {
 
@@ -16,7 +22,8 @@ Blackberry::Blackberry(const ProbeResult &device)
 	: m_dev(device.m_dev),
 	m_iface(m_dev, BLACKBERRY_INTERFACE),
 	m_pin(device.m_pin),
-	m_socket(m_dev, WRITE_ENDPOINT, READ_ENDPOINT)
+	m_socket(m_dev, WRITE_ENDPOINT, READ_ENDPOINT),
+	m_mode(Unspecified)
 {
 	if( !m_dev.SetConfiguration(BLACKBERRY_CONFIGURATION) )
 		throw SBError(m_dev.GetLastError(),
@@ -27,31 +34,235 @@ Blackberry::~Blackberry()
 {
 }
 
-void Blackberry::Test()
+void Blackberry::SelectMode(ModeType mode, uint16_t &socket, uint8_t &flag)
 {
+	// select mode
 	Packet packet;
 	packet.socket = 0;
-	packet.size = SB_MODE_PACKET_SIZE;
+	packet.size = SB_MODE_PACKET_COMMAND_SIZE;
 	packet.command = SB_COMMAND_SELECT_MODE;
-	packet.data.param.param = SB_PARAM_DEFAULT;
-	packet.data.param.data.mode.unknown1 = 0;
-	packet.data.param.data.mode.unknown2 = 0x05;	// FIXME
-	memcpy(packet.data.param.data.mode.mode_name, "RIM Desktop\0\0\0\0\0",
-		sizeof(packet.data.param.data.mode.mode_name));
+	packet.data.simple.data.mode.socket = SB_MODE_REQUEST_SOCKET;
+	packet.data.simple.data.mode.flag = 0x05;	// FIXME
+	memset(packet.data.simple.data.mode.modeName, 0,
+		sizeof(packet.data.simple.data.mode.modeName));
+
+	char *modeName = (char *) packet.data.simple.data.mode.modeName;
+	switch( mode )
+	{
+	case Bypass:
+		strcpy(modeName, "RIM Bypass");
+		break;
+
+	case Desktop:
+		strcpy(modeName, "RIM Desktop");
+		break;
+
+	case JavaLoader:
+		strcpy(modeName, "RIM_JavaLoader");
+		break;
+
+	default:
+		throw std::logic_error("Blackberry: Invalid mode in SelectMode");
+		break;
+	}
 
 	// send mode command before we open, as a default socket is socket 0
 	Data command(&packet, packet.size);
 	Data response;
 	if( !m_socket.Send(command, response) ) {
-		eout("Sent packet:\n" << command);
-		eout("Response packet:\n" << response);
+		eeout(command, response);
 		throw SBError(m_socket.GetLastStatus(),
 			"Blackberry: error setting desktop mode");
 	}
 
-	// now open our database communication socket
-	m_socket.Open(SB_SOCKET_COMM, SB_SOCKET_INIT_PARAM);
+	// get the data socket number
+	// indicates the socket number that
+	// should be used below in the Open() call
+	CheckSize(response, SB_MODE_PACKET_RESPONSE_SIZE);
+	MAKE_PACKET(modepack, response);
+	if( modepack->command != SB_COMMAND_MODE_SELECTED ) {
+		eeout(command, response);
+		throw SBError("Blackberry: mode not selected");
+	}
+
+	// return the socket and flag that the device is expecting us to use
+	socket = modepack->data.simple.data.mode.socket;
+	flag = modepack->data.simple.data.mode.flag + 1;
 }
+
+void Blackberry::OpenMode(ModeType mode)
+{
+	uint16_t socket;
+	uint8_t flag;
+
+	m_socket.Close();
+	SelectMode(mode, socket, flag);
+	m_socket.Open(socket, flag);
+}
+
+unsigned int Blackberry::GetCommand(CommandType ct)
+{
+	unsigned int cmd = 0;
+	char *cmdName = "Unknown";
+
+	switch( ct )
+	{
+	case DatabaseAccess:
+		cmdName = "Database Access";
+		cmd = m_commandTable.GetCommand(cmdName);
+		break;
+	default:
+		throw std::logic_error("Blackberry: unknown command type");
+	}
+
+	if( cmd == 0 ) {
+		std::ostringstream oss;
+		oss << "Blackberry: unable to get command code: " << cmdName;
+		throw SBError(oss.str());
+	}
+
+	return cmd;
+}
+
+void Blackberry::Test()
+{
+	// open desktop mode socket
+	OpenMode(Desktop);
+
+	// get command table
+	GetCommandTable();
+
+	// get database database
+	GetDBDB();
+
+	// get contact records
+	GetAddressBook();
+
+	// cleanup... not strictly needed, but nice to "release" the device
+	m_socket.Close();
+}
+
+void Blackberry::GetCommandTable()
+{
+	char rawCommand[] = { 6, 0, 0x0a, 0, 0x40, 0, 0, 1, 0, 0 };
+	*((uint16_t*) rawCommand) = m_socket.GetSocket();
+
+	Data command(rawCommand, sizeof(rawCommand));
+	Data response;
+	if( !m_socket.Packet(command, response) ) {
+		eeout(command, response);
+		throw SBError(m_socket.GetLastStatus(),
+			"Blackberry: error getting command table");
+	}
+
+	MAKE_PACKET(firstpack, response);
+	while( firstpack->command != SB_COMMAND_DB_DONE ) {
+		if( !m_socket.NextRecord(response) ) {
+			eout("Response packet:\n" << response);
+			throw SBError(m_socket.GetLastStatus(),
+				"Blackberry: error getting command table(next)");
+		}
+
+		MAKE_PACKET(rpack, response);
+		if( rpack->command == SB_COMMAND_DB_DATA && rpack->size > 10 ) {
+			// second packet is generally large, and contains
+			// the command table
+			m_commandTable.Clear();
+			m_commandTable.Parse(response, 6);
+		}
+	}
+
+	ddout(m_commandTable);
+}
+
+void Blackberry::GetDBDB()
+{
+	Packet packet;
+	packet.socket = m_socket.GetSocket();
+	packet.size = 7;
+	packet.command = SB_COMMAND_DB_DATA;
+	packet.data.param.param = GetCommand(DatabaseAccess);
+	packet.data.param.data.db.command = SB_DBOP_GET_DBDB;
+
+	Data command(&packet, packet.size);
+	Data response;
+
+	if( !m_socket.Packet(command, response) ) {
+		eeout(command, response);
+		throw SBError(m_socket.GetLastStatus(),
+			"Blackberry: error getting database database");
+	}
+
+	MAKE_PACKET(rpack, response);
+	while( rpack->command != SB_COMMAND_DB_DONE ) {
+		if( rpack->command == SB_COMMAND_DB_DATA ) {
+			m_dbdb.Clear();
+			m_dbdb.Parse(response, 12);	// FIXME - hardcoded
+		}
+
+		// advance!
+		if( !m_socket.NextRecord(response) ) {
+			eout("Response packet:\n" << response);
+			throw SBError(m_socket.GetLastStatus(),
+				"Blackberry: error getting command table(next)");
+		}
+		rpack = (const Packet *) response.GetData();
+	}
+
+	ddout(m_dbdb);
+}
+
+void Blackberry::GetAddressBook()
+{
+// just dumping to screen for now
+
+	unsigned int ABID = m_dbdb.GetDBNumber("Address Book");
+	// FIXME - this needs a better error handler... the dbdb needs one too!
+	if( ABID == 0 ) {
+		throw SBError("Blackberry: Address Book not found");
+	}
+
+	Packet packet;
+	packet.socket = m_socket.GetSocket();
+	packet.size = 9;
+	packet.command = SB_COMMAND_DB_DATA;
+	packet.data.param.param = GetCommand(DatabaseAccess);
+	packet.data.param.data.db.command = SB_DBOP_GET_RECORDS;
+	packet.data.param.data.db.databaseId = ABID;
+
+	Data command(&packet, packet.size);
+	Data response;
+
+	if( !m_socket.Packet(command, response) ) {
+		eeout(command, response);
+		throw SBError(m_socket.GetLastStatus(),
+			"Blackberry: error getting database database");
+	}
+
+int count = 0;
+	MAKE_PACKET(rpack, response);
+	while( rpack->command != SB_COMMAND_DB_DONE ) {
+		if( rpack->command == SB_COMMAND_DB_DATA ) {
+			Contact contact;
+			contact.Parse(response, 13);	// FIXME - hardcoded
+
+			// FIXME - do something better with the data
+			count++;
+			std::cout << contact;
+		}
+
+		// advance!
+		if( !m_socket.NextRecord(response) ) {
+			eout("Response packet:\n" << response);
+			throw SBError(m_socket.GetLastStatus(),
+				"Blackberry: error getting command table(next)");
+		}
+		rpack = (const Packet *) response.GetData();
+	}
+
+std::cout << "Contact count: " << std::setbase(10) << count;
+}
+
 
 } // namespace Syncberry
 
--- src/blackberry.h
+++ src/blackberry.h
@@ -9,22 +9,43 @@
 #include "usbwrap.h"
 #include "probe.h"
 #include "socket.h"
+#include "record.h"
 
 namespace Syncberry {
 
 class Blackberry
 {
+public:
+	enum ModeType { Unspecified, Bypass, Desktop, JavaLoader };
+	enum CommandType { Unknown, DatabaseAccess };
+
+private:
 	Usb::Device m_dev;
 	Usb::Interface m_iface;
 	uint32_t m_pin;
 
 	Socket m_socket;
 
+	CommandTable m_commandTable;
+
+	DatabaseDatabase m_dbdb;
+
+	ModeType m_mode;
+
+protected:
+	void SelectMode(ModeType mode, uint16_t &socket, uint8_t &flag);
+	void OpenMode(ModeType mode);
+	unsigned int GetCommand(CommandType ct);
+
 public:
 	Blackberry(const ProbeResult &device);
 	~Blackberry();
 
 	void Test();
+
+	void GetCommandTable();
+	void GetDBDB();
+	void GetAddressBook();
 };
 
 } // namespace Syncberry
