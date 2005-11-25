ecefba81017605f141dbf49314569533a36c4252
cdfrey <cdfrey>
2005-11-25 21:06:30 +0000

    - added barry.h for documentation purposes and application usage

---
+++ src/barry.h
@@ -0,0 +1,39 @@
+///
+/// \file	barry.h
+///		Main header file for applications
+///
+
+#ifndef __BARRY_BARRY_H__
+#define __BARRY_BARRY_H__
+
+/**
+
+	\mainpage Barry Reference Manual
+
+
+	\section getting_started Getting Started
+
+	Welcome to the Barry reference manual.  This entire manual was
+	generated via Doxygen from comments in the code.  You can view
+	the code here as well, in the Files section.
+
+	The best place to get started at the moment is to examine the
+	source code to the Barry command line tool: btool.cc
+
+
+	\section classes Major Classes
+
+	To get started with the API, see the Barry::Controller class.
+
+*/
+
+
+#include "usbwrap.h"
+#include "common.h"
+#include "error.h"
+#include "probe.h"
+#include "parser.h"
+#include "controller.h"
+
+#endif
+
