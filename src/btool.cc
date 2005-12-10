b71e53fa944a936e0e463e5f35f441753f08b5cb
cdfrey <cdfrey>
2005-12-10 07:28:21 +0000

    - added -f switch to btool, for saving and loading of record data - added -s switch to btool, for uploading saved data back to handheld

--- src/btool.cc
+++ src/btool.cc
@@ -19,14 +19,14 @@
     root directory of this project for more details.
 */
 
-
-
 #include "barry.h"
-#include <iostream>
 #include <iomanip>
+#include <iostream>
+#include <fstream>
 #include <vector>
 #include <string>
 #include <getopt.h>
+
 
 using namespace std;
 using namespace Barry;
@@ -40,10 +40,12 @@ void Usage()
    << "             specified baseDN\n"
    << "   -d db     Load database 'db' and dump to screen\n"
    << "             Can be used multiple times to fetch more than one DB\n"
+   << "   -f file   Filename to save or load handheld data to/from\n"
    << "   -h        This help\n"
    << "   -l        List devices\n"
    << "   -p pin    PIN of device to talk with\n"
    << "             If only one device plugged in, this flag is optional\n"
+   << "   -s db     Save database 'db' from data loaded from -f file\n"
    << "   -t        Show database database table\n"
    << "   -v        Dump protocol data during operation\n"
    << endl;
@@ -63,42 +65,128 @@ public:
 template <class Record>
 struct Store
 {
+	std::vector<Record> records;
+	mutable typename std::vector<Record>::const_iterator rec_it;
+	std::string filename;
+	bool load;
 	int count;
-	Store() : count(0) {}
+
+	Store(const string &filename, bool load)
+		: rec_it(records.end()),
+		filename(filename),
+		load(load),
+		count(0)
+	{
+#ifdef __BOOST_MODE__
+		try {
+
+			if( load && filename.size() ) {
+				// filename is available, attempt to save
+				cout << "Loading: " << filename << endl;
+				ifstream ifs(filename.c_str());
+				boost::archive::text_iarchive ia(ifs);
+				ia >> records;
+				cout << records.size()
+				     << " records loaded from '"
+				     << filename << "'" << endl;
+				rec_it = records.begin();
+			}
+
+		} catch( boost::archive::archive_exception &ae ) {
+			cerr << "Archive exception in ~Store(): "
+			     << ae.what() << endl;
+		}
+#endif
+	}
 	~Store()
 	{
 		cout << "Store counted " << count << " records." << endl;
+#ifdef __BOOST_MODE__
+		try {
+
+			if( !load && filename.size() ) {
+				// filename is available, attempt to save
+				cout << "Saving: " << filename << endl;
+				const std::vector<Record> &r = records;
+				ofstream ofs(filename.c_str());
+				boost::archive::text_oarchive oa(ofs);
+				oa << r;
+				cout << r.size() << " records saved to '"
+					<< filename << "'" << endl;
+			}
+
+		} catch( boost::archive::archive_exception &ae ) {
+			cerr << "Archive exception in ~Store(): "
+			     << ae.what() << endl;
+		}
+#endif
 	}
 
-	// just dump to screen for now
+	// storage operator
 	void operator()(const Record &rec)
 	{
 		count++;
 		std::cout << rec << std::endl;
+		records.push_back(rec);
+	}
+
+	// retrieval operator
+	bool operator()(Record &rec, unsigned int databaseId) const
+	{
+		if( rec_it == records.end() )
+			return false;
+		rec = *rec_it;
+		rec_it++;
+		return true;
 	}
 };
 
-auto_ptr<Parser> GetParser(const string &name)
+auto_ptr<Parser> GetParser(const string &name, const string &filename)
 {
 	// check for recognized database names
 	if( name == "Address Book" ) {
 		return auto_ptr<Parser>(
 			new RecordParser<Contact, Store<Contact> > (
-				new Store<Contact>));
+				new Store<Contact>(filename, false)));
 	}
 	else if( name == "Messages" ) {
 		return auto_ptr<Parser>(
 			new RecordParser<Message, Store<Message> > (
-				new Store<Message>));
+				new Store<Message>(filename, false)));
 	}
 	else if( name == "Calendar" ) {
 		return auto_ptr<Parser>(
 			new RecordParser<Calendar, Store<Calendar> > (
-				new Store<Calendar>));
+				new Store<Calendar>(filename, false)));
 	}
 	else {
 		// unknown database, use null parser
 		return auto_ptr<Parser>( new Parser );
+	}
+}
+
+auto_ptr<Builder> GetBuilder(const string &name, const string &filename)
+{
+	// check for recognized database names
+	if( name == "Address Book" ) {
+		return auto_ptr<Builder>(
+			new RecordBuilder<Contact, Store<Contact> > (
+				new Store<Contact>(filename, true)));
+	}
+/*
+	else if( name == "Messages" ) {
+		return auto_ptr<Parser>(
+			new RecordParser<Message, Store<Message> > (
+				new Store<Message>(filename, true)));
+	}
+	else if( name == "Calendar" ) {
+		return auto_ptr<Parser>(
+			new RecordParser<Calendar, Store<Calendar> > (
+				new Store<Calendar>(filename, true)));
+	}
+*/
+	else {
+		throw std::runtime_error("No Builder available for database");
 	}
 }
 
@@ -115,11 +203,12 @@ int main(int argc, char *argv[])
 			ldif_contacts = false,
 			data_dump = false;
 		string ldifBaseDN;
-		vector<string> dbNames;
+		string filename;
+		vector<string> dbNames, saveDbNames;
 
 		// process command line options
 		for(;;) {
-			int cmd = getopt(argc, argv, "c:d:hlp:tv");
+			int cmd = getopt(argc, argv, "c:d:f:hlp:s:tv");
 			if( cmd == -1 )
 				break;
 
@@ -134,12 +223,24 @@ int main(int argc, char *argv[])
 				dbNames.push_back(string(optarg));
 				break;
 
+			case 'f':	// filename
+#ifdef __BOOST_MODE__
+				filename = optarg;
+#else
+				cerr << "-f option not supported - no Boost "
+					"serialization support available";
+#endif
+				break;
 			case 'l':	// list only
 				list_only = true;
 				break;
 
 			case 'p':	// Blackberry PIN
 				pin = strtoul(optarg, NULL, 16);
+				break;
+
+			case 's':	// save dbname
+				saveDbNames.push_back(string(optarg));
 				break;
 
 			case 't':	// display database database
@@ -211,9 +312,21 @@ int main(int argc, char *argv[])
 
 			for( ; b != dbNames.end(); b++ ) {
 				con.OpenMode(Controller::Desktop);
+				auto_ptr<Parser> parse = GetParser(*b,filename);
 				unsigned int id = con.GetDBID(*b);
-				auto_ptr<Parser> parse = GetParser(*b);
 				con.LoadDatabase(id, *parse.get());
+			}
+		}
+
+		if( saveDbNames.size() ) {
+			vector<string>::iterator b = saveDbNames.begin();
+
+			for( ; b != saveDbNames.end(); b++ ) {
+				con.OpenMode(Controller::Desktop);
+				auto_ptr<Builder> build =
+					GetBuilder(*b, filename);
+				unsigned int id = con.GetDBID(*b);
+				con.SaveDatabase(id, *build);
 			}
 		}
 
@@ -226,6 +339,10 @@ int main(int argc, char *argv[])
 	}
 	catch( std::runtime_error &re ) {
 		std::cerr << "std::runtime_error caught: " << re.what() << endl;
+		return 1;
+	}
+	catch( std::exception &e ) {
+		std::cerr << "std::exception caught: " << e.what() << endl;
 		return 1;
 	}
 
