801c7de0facde5b58f0b5d3ddbf7d13223906ae0
cdfrey <cdfrey>
2006-09-01 20:04:15 +0000

    - added src/endian.h... still need to add configure support to 	handle non-little-endian machines

---
+++ src/endian.h
@@ -0,0 +1,36 @@
+///
+/// \file	endian.h
+///		Endian conversion macros
+///
+
+/*
+    Copyright (C) 2006, Net Direct Inc. (http://www.netdirect.ca/)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    See the GNU General Public License in the COPYING file at the
+    root directory of this project for more details.
+*/
+
+#ifndef __BARRY_ENDIAN_H__
+#define __BARRY_ENDIAN_H__
+
+// The Blackberry is little endian in its USB data.  Fortunately,
+// this makes conversion easy on the x86...
+
+#define btohs(x) x			// for uint16_t
+#define btohl(x) x			// for uint32_t
+#define btohll(x) x			// for uint64_t
+#define htobs(x) x			// for uint16_t
+#define htobl(x) x			// for uint32_t
+#define htobll(x) x			// for uint64_t
+
+#endif
+
