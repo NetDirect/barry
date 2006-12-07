45ad7cffd1f781f1203ba87dd277d6201e8db572
cdfrey <cdfrey>
2006-12-07 23:34:00 +0000

    - added a "clean" option to the buildgen.sh script... maintainer's use only

--- buildgen.sh
+++ buildgen.sh
@@ -8,7 +8,13 @@
 if [ "$1" = "orig" ] ; then
 	cp Makefile.orig Makefile
 	cp src/Makefile.orig src/Makefile
+elif [ "$1" = "clean" ] ; then
+	rm -rf autom4te.cache
+	rm -f Makefile.in aclocal.m4 config.guess config.h.in config.sub \
+		configure depcomp install-sh ltmain.sh missing \
+		src/Makefile.in tools/Makefile.in
 else
 	autoreconf -if
+	#autoreconf -ifv
 fi
 
