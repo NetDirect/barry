4d1f2171c19fdf9b76321ea98015c059f1eb1d2d
cdfrey <cdfrey>
2006-12-30 02:32:13 +0000

    - added man/ directory and bcharge.1 manpage

--- buildgen.sh
+++ buildgen.sh
@@ -8,7 +8,8 @@ if [ "$1" = "clean" ] ; then
 	rm -rf autom4te.cache
 	rm -f Makefile.in aclocal.m4 config.guess config.h.in config.sub \
 		configure depcomp install-sh ltmain.sh missing \
-		src/Makefile.in tools/Makefile.in examples/Makefile.in
+		src/Makefile.in tools/Makefile.in examples/Makefile.in \
+		man/Makefile.in
 else
 	autoreconf -if
 	#autoreconf -ifv
