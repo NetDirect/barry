ac89997933dd879251a8430db8d78e178ba50907
cdfrey <cdfrey>
2007-01-13 07:19:49 +0000

    - updated copyright dates

--- tools/btool.cc
+++ tools/btool.cc
@@ -4,7 +4,7 @@
 ///
 
 /*
-    Copyright (C) 2005-2006, Net Direct Inc. (http://www.netdirect.ca/)
+    Copyright (C) 2005-2007, Net Direct Inc. (http://www.netdirect.ca/)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -35,7 +35,7 @@ void Usage()
 {
    cerr
    << "btool - Command line USB Blackberry Test Tool\n"
-   << "        Copyright 2005-2006, Net Direct Inc. (http://www.netdirect.ca/)\n\n"
+   << "        Copyright 2005-2007, Net Direct Inc. (http://www.netdirect.ca/)\n\n"
    << "   -c dn     Convert address book database to LDIF format, using the\n"
    << "             specified baseDN\n"
    << "   -d db     Load database 'db' and dump to screen\n"
--- tools/upldif.cc
+++ tools/upldif.cc
@@ -4,7 +4,7 @@
 ///
 
 /*
-    Copyright (C) 2006, Net Direct Inc. (http://www.netdirect.ca/)
+    Copyright (C) 2006-2007, Net Direct Inc. (http://www.netdirect.ca/)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -35,7 +35,7 @@ void Usage()
 {
    cerr
    << "upldif - Command line LDIF uploader\n"
-   << "        Copyright 2006, Net Direct Inc. (http://www.netdirect.ca/)\n\n"
+   << "        Copyright 2006-2007, Net Direct Inc. (http://www.netdirect.ca/)\n\n"
    << "   -p pin    PIN of device to talk with\n"
    << "             If only one device plugged in, this flag is optional\n"
    << "   -u        Do the upload.  If not specified, only dumps parsed\n"
