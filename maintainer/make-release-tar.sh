0e2951f1cc2cad5430412fa1482cd8037ca2437d
cdfrey <cdfrey>
2007-05-02 20:16:20 +0000

    - added "HEAD" feature to tarball creation script, for easier testing

--- maintainer/make-release-tar.sh
+++ maintainer/make-release-tar.sh
@@ -14,6 +14,15 @@ echo "Using directory: $CVSREP"
 if [ -z "$1" -o -z "$2" ] ; then
 	echo
 	echo "Usage: ./make-release-tar.sh MAJOR MINOR"
+	echo "       or"
+	echo "       ./make-release-tar.sh MAJOR MINOR HEAD"
+	echo
+	echo "MAJOR is the desired major version number"
+	echo "MINOR is the desired minor version number"
+	echo "HEAD is a literal string to override the default behaviour"
+	echo "     of retrieving the CVS tagged major/minor revision, and"
+	echo "     instead, just fetching the HEAD version, and packaging"
+	echo "     it up as version MAJOR.MINOR."
 	echo
 	echo "Set the environment var CVSROOT to use a different repository."
 	echo "Set CVSREP to a different directory if needed."
@@ -29,8 +38,13 @@ cd build || exit 1
 
 set -e
 
-cvs -z3 -d "$CVSROOT" \
-	co -d $DIRNAME -r $TAGNAME "$CVSREP"
+if [ "$3" = "HEAD" ] ; then
+	cvs -z3 -d "$CVSROOT" co -d $DIRNAME -r $TAGNAME "$CVSREP"
+	echo "Fetched tag: $TAGNAME"
+else
+	cvs -z3 -d "$CVSROOT" co -d $DIRNAME "$CVSREP"
+	echo "Fetched HEAD"
+fi
 (cd $DIRNAME && ./buildgen.sh)
 (cd $DIRNAME/gui && ./buildgen.sh)
 rm -rf "$DIRNAME/autom4te.cache" "$DIRNAME/gui/autom4te.cache"
