28750290d249b64df3c3d30154218a8cf243e1c0
cdfrey <cdfrey>
2007-09-05 21:30:22 +0000

    - reordered clean commands in the buildgen.sh scripts, and removed 	the INSTALL files, since they are auto-generated by autoconf

--- buildgen.sh
+++ buildgen.sh
@@ -5,18 +5,18 @@
 #
 
 if [ "$1" = "cleanall" ] ; then
-	(cd gui && make distclean)
-	(cd gui && ./buildgen.sh clean)
-	(cd opensync-plugin && make distclean)
-	(cd opensync-plugin && ./buildgen.sh clean)
 	make distclean
+	(cd gui && make distclean)
+	(cd opensync-plugin && make distclean)
 	./buildgen.sh clean
+	(cd gui && ./buildgen.sh clean)
+	(cd opensync-plugin && ./buildgen.sh clean)
 elif [ "$1" = "clean" ] ; then
 	rm -rf autom4te.cache
 	rm -f Makefile.in aclocal.m4 config.guess config.h.in config.sub \
 		configure depcomp install-sh ltmain.sh missing \
 		src/Makefile.in tools/Makefile.in examples/Makefile.in \
-		man/Makefile.in
+		man/Makefile.in INSTALL
 	# clean up Debian build trails
 	rm -rf debian/barry
 	rm -f build-arch-stamp build-indep-stamp configure-stamp \
--- opensync-plugin/buildgen.sh
+++ opensync-plugin/buildgen.sh
@@ -9,7 +9,7 @@ if [ "$1" = "clean" ] ; then
 	rm -f Makefile.in aclocal.m4 config.guess config.h.in config.h.in~ \
 		config.sub \
 		configure depcomp install-sh ltmain.sh missing \
-		src/Makefile.in
+		src/Makefile.in INSTALL
 else
 	autoreconf -if
 	#autoreconf -ifv
