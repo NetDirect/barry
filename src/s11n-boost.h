4dffc72eb04fb63c973fb2479176fb2ab31906a5
cdfrey <cdfrey>
2008-03-06 23:27:49 +0000

    - updated s11n-boost.h header to match record classes

--- src/s11n-boost.h
+++ src/s11n-boost.h
@@ -62,6 +62,7 @@ void serialize(ArchiveT &ar, Barry::Contact::GroupLink &g, const unsigned int ve
 template <class ArchiveT>
 void serialize(ArchiveT &ar, Barry::Contact &c, const unsigned int ver)
 {
+	ar & make_nvp("RecType", c.RecType);
 	ar & make_nvp("RecordId", c.RecordId);
 
 	ar & make_nvp("Email", c.Email);
@@ -130,6 +131,19 @@ void serialize(ArchiveT &ar, Barry::Message &m, const unsigned int ver)
 	ar & make_nvp("Subject", m.Subject);
 	ar & make_nvp("Body", m.Body);
 	ar & make_nvp("Attachment", m.Attachment);
+	ar & make_nvp("MessageRecordId", m.MessageRecordId);
+	ar & make_nvp("MessageReplyTo", m.MessageReplyTo);
+	ar & make_nvp("MessageDateSent", m.MessageDateSent);
+	ar & make_nvp("MessageDateReceived", m.MessageDateReceived);
+
+	ar & make_nvp("MessageTruncated", m.MessageTruncated);
+	ar & make_nvp("MessageRead", m.MessageRead);
+	ar & make_nvp("MessageReply", m.MessageReply);
+	ar & make_nvp("MessageSaved", m.MessageSaved);
+	ar & make_nvp("MessageSavedDeleted", m.MessageSavedDeleted);
+
+	ar & make_nvp("MessagePriority", m.MessagePriority);
+	ar & make_nvp("MessageSensitivity", m.MessageSensitivity);
 
 	if( ver < BARRY_POD_MAP_VERSION ) {
 		ar & make_nvp("Unknowns", m.Unknowns);
@@ -139,6 +153,7 @@ void serialize(ArchiveT &ar, Barry::Message &m, const unsigned int ver)
 template <class ArchiveT>
 void serialize(ArchiveT &ar, Barry::Calendar &c, const unsigned int ver)
 {
+	ar & make_nvp("RecType", c.RecType);
 	ar & make_nvp("RecordId", c.RecordId);
 
 	ar & make_nvp("Recurring", c.Recurring);
@@ -168,9 +183,29 @@ void serialize(ArchiveT &ar, Barry::Calendar &c, const unsigned int ver)
 }
 
 template <class ArchiveT>
+void serialize(ArchiveT &ar, Barry::ServiceBookConfig &c, const unsigned int ver)
+{
+	ar & make_nvp("Format", c.Format);
+
+	if( ver < BARRY_POD_MAP_VERSION ) {
+		ar & make_nvp("Unknowns", c.Unknowns);
+	}
+}
+
+template <class ArchiveT>
 void serialize(ArchiveT &ar, Barry::ServiceBook &c, const unsigned int ver)
 {
+	ar & make_nvp("RecType", c.RecType);
 	ar & make_nvp("RecordId", c.RecordId);
+
+	ar & make_nvp("Name", c.Name);
+	ar & make_nvp("HiddenName", c.HiddenName);
+	ar & make_nvp("Description", c.Description);
+	ar & make_nvp("DSID", c.DSID);
+	ar & make_nvp("BesDomain", c.BesDomain);
+	ar & make_nvp("UniqueId", c.UniqueId);
+	ar & make_nvp("ContentId", c.ContentId);
+	ar & make_nvp("Config", c.Config);
 
 	if( ver < BARRY_POD_MAP_VERSION ) {
 		ar & make_nvp("Unknowns", c.Unknowns);
@@ -180,8 +215,10 @@ void serialize(ArchiveT &ar, Barry::ServiceBook &c, const unsigned int ver)
 template <class ArchiveT>
 void serialize(ArchiveT &ar, Barry::Memo &m, const unsigned int ver)
 {
+	ar & make_nvp("RecType", m.RecType);
 	ar & make_nvp("RecordId", m.RecordId);
-	
+
+	ar & make_nvp("MemoType", m.MemoType);
 	ar & make_nvp("Title", m.Title);
 	ar & make_nvp("Body", m.Body);
 	ar & make_nvp("Category", m.Category);
@@ -194,26 +231,38 @@ void serialize(ArchiveT &ar, Barry::Memo &m, const unsigned int ver)
 template <class ArchiveT>
 void serialize(ArchiveT &ar, Barry::Task &t, const unsigned int ver)
 {
+	ar & make_nvp("RecType", t.RecType);
 	ar & make_nvp("RecordId", t.RecordId);
-	
+
+	ar & make_nvp("TaskType", t.TaskType);
 	ar & make_nvp("Summary", t.Summary);
 	ar & make_nvp("Notes", t.Notes);
 	ar & make_nvp("Categories", t.Categories);
-	
+	ar & make_nvp("UID", t.UID);
+
 	ar & make_nvp("StartTime", t.StartTime);
-	ar & make_nvp("AlarmTime", t.AlarmTime);
 	ar & make_nvp("DueTime", t.DueTime);
+	ar & make_nvp("AlarmTime", t.AlarmTime);
 	
 	ar & make_nvp("TimeZoneCode", t.TimeZoneCode);
+
 	ar & make_nvp("AlarmType", t.AlarmType);
 	ar & make_nvp("Interval", t.Interval);
 	ar & make_nvp("RecurringType", t.RecurringType);
 	ar & make_nvp("RecurringEndTime", t.RecurringEndTime);
+	ar & make_nvp("DayOfWeek", t.DayOfWeek);
+	ar & make_nvp("WeekOfMonth", t.WeekOfMonth);
+	ar & make_nvp("DayOfMonth", t.DayOfMonth);
+	ar & make_nvp("MonthOfYear", t.MonthOfYear);
+	ar & make_nvp("WeekDays", t.WeekDays);
+
 	ar & make_nvp("ClassType", t.ClassType);
 	ar & make_nvp("PriorityFlag", t.PriorityFlag);
 	ar & make_nvp("StatusFlag", t.StatusFlag);
 	ar & make_nvp("Recurring", t.Recurring);
-	
+	ar & make_nvp("Perpetual", t.Perpetual);
+	ar & make_nvp("DueDateFlag", t.DueDateFlag);
+
 	if( ver < BARRY_POD_MAP_VERSION ) {
 		ar & make_nvp( "Unknowns", t.Unknowns);
 	}
@@ -222,12 +271,28 @@ void serialize(ArchiveT &ar, Barry::Task &t, const unsigned int ver)
 template<class ArchiveT>
 void serialize(ArchiveT &ar, Barry::PINMessage &p, const unsigned int ver)
 {
+	ar & make_nvp("RecType", p.RecType);
+	ar & make_nvp("RecordId", p.RecordId);
+
 	ar & make_nvp("From", p.From);
 	ar & make_nvp("To", p.To);
 	ar & make_nvp("Cc", p.Cc);
 	ar & make_nvp("Bcc", p.Bcc);
 	ar & make_nvp("Subject", p.Subject);
 	ar & make_nvp("Body", p.Body);
+	ar & make_nvp("MessageRecordId", p.MessageRecordId);
+	ar & make_nvp("MessageReplyTo", p.MessageReplyTo);
+	ar & make_nvp("MessageDateSent", p.MessageDateSent);
+	ar & make_nvp("MessageDateReceived", p.MessageDateReceived);
+
+	ar & make_nvp("MessageTruncated", p.MessageTruncated);
+	ar & make_nvp("MessageRead", p.MessageRead);
+	ar & make_nvp("MessageReply", p.MessageReply);
+	ar & make_nvp("MessageSaved", p.MessageSaved);
+	ar & make_nvp("MessageSavedDeleted", p.MessageSavedDeleted);
+
+	ar & make_nvp("MessagePriority", p.MessagePriority);
+	ar & make_nvp("MessageSensitivity", p.MessageSensitivity);
 
 	if(ver < BARRY_POD_MAP_VERSION) {
 		ar & make_nvp("Unknowns", p.Unknowns);
@@ -237,6 +302,9 @@ void serialize(ArchiveT &ar, Barry::PINMessage &p, const unsigned int ver)
 template <class ArchiveT>
 void serialize(ArchiveT &ar, Barry::SavedMessage &m, const unsigned int ver)
 {
+	ar & make_nvp("RecType", m.RecType);
+	ar & make_nvp("RecordId", m.RecordId);
+
 	ar & make_nvp("From", m.From);
 	ar & make_nvp("To", m.To);
 	ar & make_nvp("Cc", m.Cc);
@@ -246,6 +314,19 @@ void serialize(ArchiveT &ar, Barry::SavedMessage &m, const unsigned int ver)
 	ar & make_nvp("Subject", m.Subject);
 	ar & make_nvp("Body", m.Body);
 	ar & make_nvp("Attachment", m.Attachment);
+	ar & make_nvp("MessageRecordId", m.MessageRecordId);
+	ar & make_nvp("MessageReplyTo", m.MessageReplyTo);
+	ar & make_nvp("MessageDateSent", m.MessageDateSent);
+	ar & make_nvp("MessageDateReceived", m.MessageDateReceived);
+
+	ar & make_nvp("MessageTruncated", m.MessageTruncated);
+	ar & make_nvp("MessageRead", m.MessageRead);
+	ar & make_nvp("MessageReply", m.MessageReply);
+	ar & make_nvp("MessageSaved", m.MessageSaved);
+	ar & make_nvp("MessageSavedDeleted", m.MessageSavedDeleted);
+
+	ar & make_nvp("MessagePriority", m.MessagePriority);
+	ar & make_nvp("MessageSensitivity", m.MessageSensitivity);
 
 	if( ver < BARRY_POD_MAP_VERSION ) {
 		ar & make_nvp("Unknowns", m.Unknowns);
@@ -255,7 +336,14 @@ void serialize(ArchiveT &ar, Barry::SavedMessage &m, const unsigned int ver)
 template <class ArchiveT>
 void serialize(ArchiveT &ar, Barry::Folder &f, const unsigned int ver)
 {
+	ar & make_nvp("RecType", f.RecType);
+	ar & make_nvp("RecordId", f.RecordId);
+
 	ar & make_nvp("FolderName", f.FolderName);
+	ar & make_nvp("FolderNumber", f.FolderNumber);
+	ar & make_nvp("FolderLevel", f.FolderLevel);
+	ar & make_nvp("FolderType", f.FolderType);
+
 	if( ver < BARRY_POD_MAP_VERSION ) {
 		ar & make_nvp( "Unknowns", f.Unknowns);
 	}
@@ -264,17 +352,22 @@ void serialize(ArchiveT &ar, Barry::Folder &f, const unsigned int ver)
 template <class ArchiveT>
 void serialize(ArchiveT &ar, Barry::Timezone &t, const unsigned int ver)
 {
+	ar & make_nvp("RecType", t.RecType);
+	ar & make_nvp("RecordId", t.RecordId);
+
+	ar & make_nvp("TZType", t.TZType);
+	ar & make_nvp("DSTOffset", t.DSTOffset);
 	ar & make_nvp("Index", t.Index);
-	ar & make_nvp("TimeZoneName", t.TimeZoneName);
 	ar & make_nvp("Offset", t.Offset);
 	ar & make_nvp("OffsetFraction", t.OffsetFraction);
-	ar & make_nvp("Left", t.Left);
-	ar & make_nvp("UseDST", t.UseDST);
-	ar & make_nvp("DSTOffset", t.DSTOffset);
 	ar & make_nvp("StartMonth", t.StartMonth);
 	ar & make_nvp("EndMonth", t.EndMonth);
+	ar & make_nvp("Left", t.Left);
+	ar & make_nvp("UseDST", t.UseDST);
 
-	if( ver < BARRY_POD_MAP_VERSION) {
+	ar & make_nvp("TimeZoneName", t.TimeZoneName);
+
+	if( ver < BARRY_POD_MAP_VERSION ) {
 		ar & make_nvp("Unknowns", t.Unknowns);
 	}
 }
