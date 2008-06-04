e398b00d77422898dc8d00e3d0f950311b6a1188
Chris Frey <cdfrey@foursquare.net>
2008-06-04 17:49:45 -0400

    Fixed missing and mis-dated copyright notices in src/

--- src/pppfilter.cc
+++ src/pppfilter.cc
@@ -6,6 +6,22 @@
 ///		gprs_protocol_fix.c program.
 ///
 
+/*
+    Copyright (C) 2008, Net Direct Inc. (http://www.netdirect.ca/)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    See the GNU General Public License in the COPYING file at the
+    root directory of this project for more details.
+*/
+
 #include "pppfilter.h"
 #include <string.h>
 
