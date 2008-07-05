a73d91134c9560cab78761737e96be4baae788dc
Chris Frey <cdfrey@foursquare.net>
2008-07-04 23:14:06 -0400

    Updated release scripts to build RPMs with the new spec file

--- maintainer/release-root.sh
+++ maintainer/release-root.sh
@@ -9,13 +9,24 @@ fi
 
 set -e
 
+#
 # Build the DEB's
+#
 ./make-deb.sh build/barry-$1.$2.tar.bz2 ubuntu804 ubuntu804
 ./make-deb.sh build/barry-$1.$2.tar.bz2 ubuntu710 ubuntu710
-# Build the RPM's
+
+
+#
+# Build the Fedora RPM's
+#
+./make-rpm.sh build/barry-$1.$2.tar.bz2 ../rpm/barry.spec fedora9 fc9
+./make-rpm.sh build/barry-$1.$2.tar.bz2 ../rpm/barry.spec fedora8 fc8
 ./make-rpm.sh build/barry-$1.$2.tar.bz2 ../rpm/barry.spec fedora7 fc7
-sed "s/libusb-devel/libusb/g;s/gtkmm24/gtkmm2/g;s/libglademm24/libglademm/g" < ../rpm/barry.spec > barry-opensuse.spec
-./make-rpm.sh build/barry-$1.$2.tar.bz2 barry-opensuse.spec opensuse10.2 suse10
-./make-rpm.sh build/barry-$1.$2.tar.bz2 ../rpm/barry.spec fedora6 fc6
+#./make-rpm.sh build/barry-$1.$2.tar.bz2 ../rpm/barry.spec fedora6 fc6
 #./make-rpm.sh build/barry-$1.$2.tar.bz2 ../rpm/barry.spec fedora5 fc5
+
+#
+# Build the OpenSUSE RPM's
+#
+./make-rpm.sh build/barry-$1.$2.tar.bz2 ../rpm/barry.spec opensuse10.2 suse10
 
