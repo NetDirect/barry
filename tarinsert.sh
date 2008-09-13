2deae45de4c1a3cf9ee253cae4a50c29ed7842b0
Chris Frey <cdfrey@foursquare.net>
2008-09-12 23:05:23 -0400

    Added tarinsert.sh, for adding tagged tarball trees to git

---
+++ tarinsert.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+if [ -z "$1" ] ; then
+	echo "Usage: tarinsert.sh compress_option tarball tag "
+	echo
+	echo "Example:  tarinsert.sh -z barry-0.0.1.tar.gz barry-0.0.1"
+	echo
+	exit 1
+fi
+
+set -e
+set -x
+
+git checkout -b tarinsert $3
+git rm -r .
+tar $1 -xvf $2
+mv $3/* .
+
+set +e
+mv $(find $3 -type f -print) .
+set -e
+
+rmdir $3
+git add .
+
+set +e
+git status
+git commit -m "Release tarball for $3"
+set -e
+
+git tag $(basename $2)
+git checkout master
+git branch -D tarinsert
+echo "Done!"
+
