6dcaaeb171a920c36959eda6ed01220f92270f53
Chris Frey <cdfrey@foursquare.net>
2009-04-02 19:10:02 -0400

    Added opensync 0.4x to "config all" script

--- configure-barryall.sh
+++ configure-barryall.sh
@@ -8,9 +8,11 @@ export CXX="ccache g++"
 #export CXXFLAGS="-Wall -Werror -pedantic -O0 -g"
 export CXXFLAGS="-Wall -Werror -O0 -g"
 #export PKG_CONFIG_PATH=/home/cdfrey/Contract/netdirect/syncberry/cvs/barry1/rootdir/lib/pkgconfig:/home/cdfrey/software/opensync/0.22/rootdir/lib/pkgconfig
+export PKG_CONFIG_PATH=/home/cdfrey/software/opensync/git/rootdir/lib/pkgconfig
 
 ./configure \
 	--prefix=/home/cdfrey/Contract/netdirect/syncberry/cvs/barry1/rootdir \
 	--enable-gui \
-	--enable-opensync-plugin
+	--enable-opensync-plugin \
+	--enable-opensync-plugin-4x
 
