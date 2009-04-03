a86d9e96c1c05656764ce7e57c8e4f9507853553
Ryan Li <ryan@ryanium.com>
2009-04-03 16:14:39 -0400

    Applied Ryan Li's SMS record parsing and doc patch
    From his email:
    
    Hello Chris,
    
    I've been working on the sms parser for Barry, the code is raw, since the
    data structure for SMS is really strange and I can't use a packed struct to
    handle this effectively. But at least, it works now. And I'll do some
    refinement later when I have finished my homework.
    
    However, I've checked the common.h file and found that the iconv.cc in Barry
    converts WINDOWS-1252 to UTF-8, but with SMS encoded as UCS-2, btool
    displays my Chinese messages as garbled text. Furthermore, after I changed
    the definition of BLACKBERRY_CHARSET to `UCS-2', the output doesn't change.
    I thought there's something wrong with iconv.cc? My Memo in 100% English
    can't be interpreted correctly either.
    
    Nevertheless, it should work for Western languages, copy the two source
    files to /src and modify /tools/btool.cc, /src/record.h, /src/Makefile.am.
    Make install and run btool.
    
    r_sms.cc, r_sms.h and bb_sms_format.txt attached.
    
    Best Regards,
    Ryan Li

---
+++ src/r_sms.cc
@@ -0,0 +1,209 @@
+///
+/// \file	r_sms.cc
+///		Record parsing class for the SMS database.
+///
+
+/*
+    Copyright (C) 2005-2009, Net Direct Inc. (http://www.netdirect.ca/)
+    Copyright (C) 2009, Ryan Li(ryan@ryanium.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    See the GNU General Public License in the COPYING file at the
+    root directory of this project for more details.
+*/
+
+#include "r_sms.h"
+#include "record-internal.h"
+#include "protostructs.h"
+#include "data.h"
+#include "time.h"
+#include "debug.h"
+#include "iconv.h"
+#include <ostream>
+#include <iomanip>
+#include <string.h>
+
+using namespace std;
+using namespace Barry::Protocol;
+
+namespace Barry {
+
+///////////////////////////////////////////////////////////////////////////////
+// SMS Class
+
+// SMS Field Codes
+#define SMSFC_METADATA 0x01
+#define SMSFC_PHONE_NUMBER 0x02
+#define SMSFC_CONTENT 0x04
+#define SMSFC_END 0xffff
+
+static FieldLink<SMS> SMSFieldLinks[] = {
+	{SMSFC_CONTENT, "Content", 0, 0, &SMS::Content, 0, 0, 0, 0, true},
+	{SMSFC_END, "End of List", 0, 0, 0, 0, 0, 0, 0, false},
+};
+
+SMS::SMS()
+{
+	Clear();
+}
+
+SMS::~SMS()
+{
+}
+
+const unsigned char* SMS::ParseField(const unsigned char *begin,
+				     const unsigned char *end,
+				     const IConverter *ic)
+{
+	const CommonField *field = (const CommonField *)begin;
+
+	// advance and check size
+	begin += COMMON_FIELD_HEADER_SIZE + btohs(field->size);
+	if (begin > end)       // if begin==end, we are ok
+		return begin;
+
+	if (!btohs(field->size))   // if field has no size, something's up
+		return begin;
+
+	// cycle through the type table
+	for(FieldLink<SMS> *b = SMSFieldLinks;
+		b->type != SMSFC_END;
+		b++)
+		if (b->type == field->type)
+			if (b->strMember) {
+				std::string &s = this->*(b->strMember);
+				s = ParseFieldString(field);
+				if (b->iconvNeeded && ic)
+					s = ic->FromBB(s);
+				return begin;   // done!
+			}
+	// handle special cases
+	switch (field->type)
+	{
+		case SMSFC_METADATA:
+		{
+			const unsigned char *str = (const unsigned char *)field->u.raw;
+			NewConversation = str[1] & 0x20;
+			Saved = str[1] & 0x10;
+			Deleted = str[1] & 0x08;
+			Opened = str[1] & 0x01;
+			IsNew = str[2];
+			if (*((uint32_t *) (str + 5)) == 0x000007ff)
+				MessageStatus = Received;
+			else if (*((uint32_t *) (str + 5)) == 0x7fffffff)
+				MessageStatus = Draft;
+			else
+				MessageStatus = Sent; //consider all others as sent.
+			
+			ErrorId = *((uint32_t *) (str + 9));
+			
+			TimeStamp = *((uint64_t *) (str + 13));
+			
+			SentTimeStamp += *((uint64_t *) (str + 21));
+			
+			Encoding = (EncodingType)str[29];
+			
+			return begin;
+		}
+		case SMSFC_PHONE_NUMBER:
+			const char *str = (const char *)field->u.raw;
+			uint16_t len = btohs(field->size);
+			PhoneNumbers.push_back(std::string(str + 4, len - 4));
+			return begin;
+	}
+
+	// if still not handled, add to the Unknowns list
+	UnknownField uf;
+	uf.type = field->type;
+	uf.data.assign((const char*)field->u.raw, btohs(field->size));
+	Unknowns.push_back(uf);
+
+	// return new pointer for next field
+	return begin;
+}
+
+void SMS::ParseHeader(const Data &data, size_t &offset)
+{
+	// no header in SMS records
+}
+
+void SMS::ParseFields(const Data &data, size_t &offset, const IConverter *ic)
+{
+	const unsigned char *finish = ParseCommonFields(*this,
+		data.GetData() + offset, data.GetData() + data.GetSize(), ic);
+	offset += finish - (data.GetData() + offset);
+}
+
+void SMS::Clear()
+{
+	MessageStatus = (MessageType)0;
+	DeliveryStatus = (DeliveryType)0;
+	Encoding = (EncodingType)0;
+	
+	IsNew = NewConversation = Saved = Deleted = Opened = false;
+	
+	TimeStamp = SentTimeStamp = 0;
+	ErrorId = 0;
+	
+	PhoneNumbers.clear();
+	Content.clear();
+	
+	Unknowns.clear();
+}
+
+void SMS::Dump(std::ostream &os) const
+{
+
+	os << "Task entry: 0x" << setbase(16) << RecordId
+		<< " (" << (unsigned int)RecType << ")\n";
+	time_t t = TimeStamp / 1000;
+	os << "Time: " << ctime(&t);
+	if (MessageStatus == Received)
+	{
+		t = SentTimeStamp / 1000;
+		os << "Sent at: " << ctime(&t);
+		// FIXME - since the ISP may use a time zone other than UTC, this time probably has an offset.
+	}
+	if (ErrorId)
+		os << "Send Error: 0x" << setbase(16) << ErrorId << "\n";
+	switch (MessageStatus)
+	{
+		case Received:
+			os << "Received From:" << "\n";
+			break;
+		case Sent:
+			os << "Sent to:" << "\n";
+			break;
+		case Draft:
+			os << "Draft for:" << "\n";
+			break;
+	}
+	for (std::vector<std::string>::const_iterator Iterator = PhoneNumbers.begin(); Iterator < PhoneNumbers.end(); ++Iterator)
+		os << *Iterator << "\n";
+	if (IsNew)
+		os << "New ";
+	if (Opened)
+		os << "Opened ";
+	if (Saved)
+		os << "Saved ";
+	if (Deleted)
+		os << "Deleted ";
+	if (IsNew || Opened || Saved || Deleted || NewConversation)
+		os << "Message";
+	if (NewConversation)
+		os << "that starts a new conversation";
+	os << "\n";
+	os << "Content:" << Content << "\n";
+	os << "\n";
+}
+
+} // namespace Barry
+
---
+++ src/r_sms.h
@@ -0,0 +1,114 @@
+///
+/// \file	r_task.h
+///		Record parsing class for the task database.
+///
+
+/*
+    Copyright (C) 2005-2009, Net Direct Inc. (http://www.netdirect.ca/)
+    Copyright (C) 2009, Ryan Li(ryan@ryanium.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    See the GNU General Public License in the COPYING file at the
+    root directory of this project for more details.
+*/
+
+#ifndef __BARRY_RECORD_SMS_H__
+#define __BARRY_RECORD_SMS_H__
+
+#include "dll.h"
+#include "record.h"
+#include <vector>
+#include <string>
+#include <stdint.h>
+
+namespace Barry {
+
+// forward declarations
+class IConverter;
+
+class BXEXPORT SMS
+{
+public:
+	typedef std::vector<UnknownField> UnknownsType;
+	
+	uint8_t RecType;
+	uint32_t RecordId;
+
+	enum MessageType
+	{
+		Received = 0,
+		Sent,
+		Draft
+	};
+	MessageType MessageStatus;
+	
+	enum DeliveryType
+	{
+		NoReport = 0,
+		Failed,
+		Succedded
+	};
+	DeliveryType DeliveryStatus;
+	
+	bool IsNew;
+	bool NewConversation;
+	bool Saved;
+	bool Deleted;
+	bool Opened;
+	
+	uint64_t TimeStamp;
+	uint64_t SentTimeStamp;
+	
+	enum EncodingType
+	{
+		SevenBit = 0,
+		VCard,
+		UCS2
+	};
+	EncodingType Encoding;
+	
+	uint32_t ErrorId;
+	
+	std::vector<std::string> PhoneNumbers;
+	std::string Content;
+	
+	UnknownsType Unknowns;
+
+public:
+	SMS();
+	~SMS();
+
+	const unsigned char* ParseField(const unsigned char *begin, const unsigned char *end, const IConverter *ic = 0);
+	uint8_t GetRecType() const { return RecType; }
+	uint32_t GetUniqueId() const { return RecordId; }
+	void SetIds(uint8_t Type, uint32_t Id) { RecType = Type; RecordId = Id; }
+	void ParseHeader(const Data &data, size_t &offset);
+	void ParseFields(const Data &data, size_t &offset, const IConverter *ic = 0);
+	void BuildHeader(Data &data, size_t &offset) const;
+
+	void Clear();
+
+	void Dump(std::ostream &os) const;
+
+	// database name
+	static const char * GetDBName() { return "SMS Messages"; }
+	static uint8_t GetDefaultRecType() { return 5; }
+};
+
+BXEXPORT inline std::ostream& operator<<(std::ostream &os, const SMS &msg) {
+	msg.Dump(os);
+	return os;
+}
+
+} // namespace Barry
+
+#endif
+
