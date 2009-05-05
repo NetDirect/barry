759f28d0ce840da9a45cdada12c95da6f9db8b6d
Nicolas VIVIEN <nicolas@vivien.fr>
2009-05-05 13:13:34 +0200

    Invert StartTime and DueTime field.
    /!\ On my BlackBerry the StartTime field isn't displayed, I can see this
    field only in using btool command. I notice also that if I update the
    DueTime field of my task, the StartTime field is also updated with the
    same value !

--- src/r_task.cc
+++ src/r_task.cc
@@ -87,8 +87,8 @@ uint8_t Task::StatusRec2Proto(StatusFlagType s)
 #define TSKFC_TASK_TYPE		0x01
 #define TSKFC_TITLE		0x02
 #define TSKFC_NOTES		0x03
-#define TSKFC_START_TIME	0x05
-#define TSKFC_DUE_TIME		0x06
+#define TSKFC_DUE_TIME		0x05
+#define TSKFC_START_TIME	0x06
 #define TSKFC_DUE_FLAG		0x08
 #define TSKFC_STATUS		0x09
 #define TSKFC_PRIORITY		0x0a
