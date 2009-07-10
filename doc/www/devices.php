1f593fd878f7213378aa2fc7675342a5e2568d90
Chris Frey <cdfrey@foursquare.net>
2009-07-10 09:35:01 -0400

    Started device compatibility list in web docs

---
+++ doc/www/devices.php
@@ -0,0 +1,35 @@
+<? createHeader("Device Compatibility List"); ?>
+
+<? include ("barry.inc"); ?>
+
+
+<div class="subHeader">Device Compatibility List</div>
+
+<p>This list is intended as a rough guide and record of past success
+when using Barry with various versions of the BlackBerry handheld.
+It is not guaranteed to be complete, and is based on feedback from
+users such as yourself.</p>
+
+<p>If you wish to contribute a report, please send
+it to the <? createLink("contact", "mailing list"); ?>.</p>
+
+<table border="1">
+<tr>
+	<th>Device Model</th>
+	<th>Firmware Version</th>
+	<th>Barry Version</th>
+	<th>Distro / Machine</th>
+	<th>Backup/Restore Databases</th>
+	<th>Device Charges?</th>
+	<th>Reported By</th>
+</tr>
+<tr>
+	<td>Storm 9530</td>
+	<td>?</td>
+	<td>0.15-git20090710</td>
+	<td>Debian Sid on Intel Core Duo 32 bits</td>
+	<td>?</td>
+	<td>?</td>
+	<td><a href="http://www.mail-archive.com/barry-devel@lists.sourceforge.net/msg02373.html">Nicolas Vivien</a></td>
+</table>
+
