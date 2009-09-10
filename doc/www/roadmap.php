b362cfad294ab7272b450260d4a2cc8db4c320c7
Chris Frey <cdfrey@foursquare.net>
2009-09-10 15:01:23 -0400

    www: updated roadmap.php with new release schedule

--- doc/www/roadmap.php
+++ doc/www/roadmap.php
@@ -7,35 +7,35 @@
 in the source tree, and while relatively up to date, the source tree is the
 definitive copy.</p>
 
-<? createSubHeader("Target: release version 0.16 (Sept 3)"); ?>
+<? createSubHeader("Target: release version 0.16 (Oct 1)"); ?>
 <ul>
+	<li>incorporate barrybackup GUI changes by Ryan Li (done)</li>
 	<li>incorporate Nicolas's java debugger patches</li>
-	<li>incorporate barrybackup GUI changes by Ryan Li</li>
+	<li>incorporate Martin Owens' Barry logo/icon into Barry</li>
 </ul>
 
-<? createSubHeader("Target: release version 0.17 (Oct 1)"); ?>
+<? createSubHeader("Target: release version 0.17 (Nov 1)"); ?>
 <ul>
-<li>test and fix all build and functionality issues on:</li>
-	<ul>
-		<li>Fedora 11</li>
-		<li>Ubuntu 8.10, 9.04</li>
-		<li>openSUSE 11.1</li>
-		<li>openBSD</li>
-	</ul>
-<li>website documentation:</li>
-	<ul>
-		<li>add a more detailed set of instructions for how to
-			contribute to the project using git</li>
-		<li>incorporate Bill Paul's modem HOWTO for FreeBSD
-			into web docs</li>
-		<li>add docs for opensync 0.4x plugin</li>
-	</ul>
+	<li>website documentation</li>
 </ul>
 
-<? createSubHeader("Target: release version 0.18 (Nov 13)"); ?>
+<? createSubHeader("Target: release version 0.18 (Dec 1)"); ?>
+<ul>
+	<li>test and fix all build and functionality issues on:</li>
+		<ul>
+			<li>Fedora 11</li>
+			<li>Fedora 12 (scheduled release on Nov 10)</li>
+			<li>Ubuntu 8.10, 9.04</li>
+			<li>Ubuntu 9.10 (scheduled release on Oct 29)</li>
+			<li>openSUSE 11.1</li>
+			<li>openBSD</li>
+		</ul>
+	<li>support our own repositories for apt/yum/zypper installs</li>
+</ul>
+
+<? createSubHeader("Target: release version 0.19 (Jan 1, 2010)"); ?>
 <ul>
 	<li>polish up bfuse, and add feature to split out fields</li>
-	<li>add record classes for Content Store, SMS Messages, based on
-		Martin Owens' doc/barry-research.ods</li>
+	<li>add record classes for Content Store</li>
 </ul>
 
