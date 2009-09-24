5345632346d7e3168adf3c6d5c4f847939cbb520
Chris Frey <cdfrey@foursquare.net>
2009-09-24 17:09:06 -0400

    desktop: added skeleton build system

---
+++ desktop/buildgen.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#
+# Generates the build system.
+#
+
+if [ "$1" = "clean" ] ; then
+	rm -rf autom4te.cache
+	rm -f Makefile.in aclocal.m4 config.guess config.h.in config.sub \
+		configure depcomp install-sh ltmain.sh missing \
+		man/Makefile.in \
+		src/Makefile.in src/*.bak \
+		INSTALL \
+		config.h.in~
+else
+	autoreconf -if
+	#autoreconf -ifv
+fi
+
