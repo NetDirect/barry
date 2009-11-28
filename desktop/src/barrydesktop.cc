93a0d084ce202bb97cb93ff508fb92171ff13070
Chris Frey <cdfrey@foursquare.net>
2009-11-27 19:10:30 -0500

    desktop: made the Barry logo clickable for the main system menu

--- desktop/src/barrydesktop.cc
+++ desktop/src/barrydesktop.cc
@@ -36,6 +36,9 @@ using namespace std;
 //////////////////////////////////////////////////////////////////////////////
 // IDs for controls and menu items (no menus in this app yet)
 enum {
+	SysMenu_Exit = wxID_EXIT,
+	SysMenu_About = wxID_ABOUT,
+
 	MainMenu_FirstButton = wxID_HIGHEST,
 
 	MainMenu_BackupAndRestore = MainMenu_FirstButton,
@@ -47,7 +50,10 @@ enum {
 	MainMenu_MediaManagement,
 	MainMenu_Misc,
 
-	MainMenu_LastButton = MainMenu_Misc
+	MainMenu_LastButton = MainMenu_Misc,
+
+	// Clickable, "hot" images that do something
+	HotImage_BarryLogo
 };
 
 const wxChar *ButtonNames[] = {
@@ -82,6 +88,27 @@ wxString GetButtonFilename(int id, int state)
 	file += StateNames[state];
 	return file;
 }
+
+class ClickableImage
+{
+	wxWindow *m_parent;
+	int m_id;
+	wxBitmap m_image;
+	int m_x, m_y;
+	bool m_focus;
+
+protected:
+	bool CalculateHit(int x, int y);
+
+public:
+	ClickableImage(wxWindow *parent, const wxBitmap &image,
+		int ID, int x, int y);
+
+	void Draw(wxDC &dc);
+	void HandleMotion(wxDC &dc, int x, int y);
+	void HandleDown(wxDC &dc, int x, int y);
+	void HandleUp(wxDC &dc, int x, int y);
+};
 
 class PNGButton
 {
@@ -137,8 +164,9 @@ private:
 private:
 	std::auto_ptr<wxBitmap> m_background, m_button;
 	std::auto_ptr<BaseButtons> m_basebuttons;
+	std::auto_ptr<ClickableImage> m_barry_logo;
+	std::auto_ptr<wxMenu> m_sysmenu;
 	int m_width, m_height;
-	wxBitmap m_barry_logo;
 
 public:
 	BaseFrame(const wxImage &background);
@@ -148,6 +176,11 @@ public:
 	void OnLeftDown(wxMouseEvent &event);
 	void OnLeftUp(wxMouseEvent &event);
 	void OnBackupRestore(wxCommandEvent &event);
+	void OnBarryLogoClicked(wxCommandEvent &event);
+
+	// sys menu (triggered by the Barry logo)
+	void OnAbout(wxCommandEvent &event);
+	void OnExit(wxCommandEvent &event);
 };
 
 BEGIN_EVENT_TABLE(BaseFrame, wxFrame)
@@ -156,6 +189,9 @@ BEGIN_EVENT_TABLE(BaseFrame, wxFrame)
 	EVT_LEFT_DOWN	(BaseFrame::OnLeftDown)
 	EVT_LEFT_UP	(BaseFrame::OnLeftUp)
 	EVT_BUTTON	(MainMenu_BackupAndRestore, BaseFrame::OnBackupRestore)
+	EVT_BUTTON	(HotImage_BarryLogo, BaseFrame::OnBarryLogoClicked)
+	EVT_MENU	(SysMenu_About, BaseFrame::OnAbout)
+	EVT_MENU	(SysMenu_Exit, BaseFrame::OnExit)
 END_EVENT_TABLE()
 
 class BarryDesktopApp : public wxApp
@@ -164,6 +200,71 @@ public:
 	virtual bool OnInit();
 	virtual int OnExit();
 };
+
+//////////////////////////////////////////////////////////////////////////////
+// ClickableImage
+
+ClickableImage::ClickableImage(wxWindow *parent,
+				const wxBitmap &image,
+				int ID,
+				int x, int y)
+	: m_parent(parent)
+	, m_id(ID)
+	, m_image(image)
+	, m_x(x)
+	, m_y(y)
+	, m_focus(false)
+{
+}
+
+bool ClickableImage::CalculateHit(int x, int y)
+{
+	return  ( x >= m_x && x < (m_x + m_image.GetWidth()) )
+		&&
+		( y >= m_y && y < (m_y + m_image.GetHeight()) );
+}
+
+void ClickableImage::Draw(wxDC &dc)
+{
+	dc.DrawBitmap(m_image, m_x, m_y, true);
+}
+
+void ClickableImage::HandleMotion(wxDC &dc, int x, int y)
+{
+	bool focus = CalculateHit(x, y);
+
+	if( focus && !m_focus ) {
+		// newly in focus
+		m_parent->SetCursor(wxCursor(wxCURSOR_HAND));
+	}
+	else if( m_focus && !focus ) {
+		// not in focus anymore
+		m_parent->SetCursor(wxNullCursor);
+	}
+
+	// remember state
+	m_focus = focus;
+}
+
+void ClickableImage::HandleDown(wxDC &dc, int x, int y)
+{
+}
+
+void ClickableImage::HandleUp(wxDC &dc, int x, int y)
+{
+	m_focus = CalculateHit(x, y);
+
+	if( m_focus ) {
+		// replace the cursor
+		m_parent->SetCursor(wxNullCursor);
+		m_focus = false;
+
+		// send the event
+		wxCommandEvent event(wxEVT_COMMAND_BUTTON_CLICKED, m_id);
+		m_parent->GetEventHandler()->ProcessEvent(event);
+	}
+}
+
 
 //////////////////////////////////////////////////////////////////////////////
 // PNGButton
@@ -355,10 +456,17 @@ BaseFrame::BaseFrame(const wxImage &background)
 		wxCLIP_CHILDREN)
 	, m_width(background.GetWidth())
 	, m_height(background.GetHeight())
-	, m_barry_logo(barry_logo_icon_xpm)
 {
 	m_background.reset( new wxBitmap(background) );
 	m_basebuttons.reset( new BaseButtons(this) );
+	m_barry_logo.reset( new ClickableImage(this,
+		wxBitmap(barry_logo_icon_xpm), HotImage_BarryLogo, 4, 4) );
+
+	// Create the Barry Logo popup system menu
+	m_sysmenu.reset( new wxMenu );
+	m_sysmenu->Append(SysMenu_About, _T("&About..."));
+	m_sysmenu->AppendSeparator();
+	m_sysmenu->Append(wxID_EXIT, _T("E&xit"));
 }
 
 void BaseFrame::OnPaint(wxPaintEvent &event)
@@ -370,7 +478,7 @@ static bool init = false;
 	dc.DrawBitmap(*m_background, 0, 0);
 
 	// paint the header: Barry logo
-	dc.DrawBitmap(m_barry_logo, 4, 4, true);
+	m_barry_logo->Draw(dc);
 
 	// paint the header: NetDirect logo
 	// FIXME
@@ -402,12 +510,14 @@ void BaseFrame::OnMouseMotion(wxMouseEvent &event)
 {
 	wxClientDC dc(this);
 	m_basebuttons->HandleMotion(dc, event.m_x, event.m_y);
+	m_barry_logo->HandleMotion(dc, event.m_x, event.m_y);
 }
 
 void BaseFrame::OnLeftDown(wxMouseEvent &event)
 {
 	wxClientDC dc(this);
 	m_basebuttons->HandleDown(dc, event.m_x, event.m_y);
+	m_barry_logo->HandleDown(dc, event.m_x, event.m_y);
 	event.Skip();
 }
 
@@ -415,11 +525,27 @@ void BaseFrame::OnLeftUp(wxMouseEvent &event)
 {
 	wxClientDC dc(this);
 	m_basebuttons->HandleUp(dc, event.m_x, event.m_y);
+	m_barry_logo->HandleUp(dc, event.m_x, event.m_y);
 }
 
 void BaseFrame::OnBackupRestore(wxCommandEvent &event)
 {
 	wxMessageBox(_T("OnBackupRestore"));
+}
+
+void BaseFrame::OnBarryLogoClicked(wxCommandEvent &event)
+{
+	PopupMenu(m_sysmenu.get(), 4, 20);
+}
+
+void BaseFrame::OnAbout(wxCommandEvent &event)
+{
+	wxMessageBox(_T("FIXME: About box needs to be implemented"));
+}
+
+void BaseFrame::OnExit(wxCommandEvent &event)
+{
+	Close(true);
 }
 
 //////////////////////////////////////////////////////////////////////////////
