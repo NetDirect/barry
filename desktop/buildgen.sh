fd58b045747bb1c85c7fffedc58f981e9737d35b
Chris Frey <cdfrey@foursquare.net>
2009-12-03 15:51:08 -0500

    desktop: added images/ to autoconf
    Still need to install the images somewhere, similar to glade files.
    This is just a stopgap measure to make the build work.

--- desktop/buildgen.sh
+++ desktop/buildgen.sh
@@ -8,6 +8,7 @@ if [ "$1" = "clean" ] ; then
 	rm -rf autom4te.cache
 	rm -f Makefile.in aclocal.m4 config.guess config.h.in config.sub \
 		configure depcomp install-sh ltmain.sh missing \
+		images/Makefile.in \
 		man/Makefile.in \
 		src/Makefile.in src/*.bak \
 		INSTALL \
