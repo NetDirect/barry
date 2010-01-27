ad5c48b872b105250120f81ffae73384c1c9bb6a
Chris Frey <cdfrey@foursquare.net>
2010-01-26 20:54:57 -0500

    Added F12 build target to maintainer scripts

--- maintainer/release-root.sh
+++ maintainer/release-root.sh
@@ -20,6 +20,7 @@ set -e
 #
 # Build the Fedora RPM's
 #
+./make-user-rpm.sh build/barry-$1.$2.tar.bz2 ../rpm/barry.spec fedora12 f12
 ./make-user-rpm.sh build/barry-$1.$2.tar.bz2 ../rpm/barry.spec fedora11 f11
 ./make-rpm.sh build/barry-$1.$2.tar.bz2 ../rpm/barry.spec fedora9 fc9
 ./make-rpm.sh build/barry-$1.$2.tar.bz2 ../rpm/barry.spec fedora8 fc8
