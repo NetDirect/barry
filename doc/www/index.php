7ad09168a07b13f219586fe3843555e75e8e79cf
Chris Frey <cdfrey@foursquare.net>
2010-03-30 22:42:04 -0400

    Updated Fedora and Mandriva maintainer info

--- doc/www/index.php
+++ doc/www/index.php
@@ -54,8 +54,8 @@ reverse engineer the battery charging handshake via USB.</p>
 	<li>Additional binary packages for the following distributions are available from various volunteers:
 		<ul>
 			<li><a href="http://packages.debian.org/unstable/main/barry-util">Debian, in the unstable tree, packaged by Jose Carlos Garcia Sogo</a></li>
-			<li><a href="http://koji.fedoraproject.org/koji/packageinfo?packageID=7118">Fedora, packaged by Christopher D. Stover</a>, and the related RPM <a href="http://cvs.fedoraproject.org/viewvc/rpms/barry/">CVS</a></li>
-			<li>Mandriva, packaged by Adam Williamson, and the related RPM <a href="http://svn.mandriva.com/cgi-bin/viewvc.cgi/packages/cooker/barry/current/">SVN</a></li>
+			<li><a href="http://koji.fedoraproject.org/koji/packageinfo?packageID=7118">Fedora</a> (packaged by Nathanael Noblet) and the related RPM <a href="http://cvs.fedoraproject.org/viewvc/rpms/barry/">CVS</a></li>
+			<li>Mandriva's RPM <a href="http://svn.mandriva.com/cgi-bin/viewvc.cgi/packages/cooker/barry/current/">SVN</a></li>
 			<li><a href="https://launchpad.net/~doctormo/+archive/barry-snapshot">Ubuntu, packaged by Martin Owens</a></li>
 		</ul>
 	</li>
