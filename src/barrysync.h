e073ae6505f2ed00b7c609a6dce3f3d27c679e7e
Chris Frey <cdfrey@foursquare.net>
2010-07-17 00:12:27 -0400

    lib: added tzwrapper to build, and created new barrysync.h header

---
+++ src/barrysync.h
@@ -0,0 +1,32 @@
+///
+/// \file	barrysync.h
+///		Main header file for libbarrysync
+///
+
+/*
+    Copyright (C) 2010, Net Direct Inc. (http://www.netdirect.ca/)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    See the GNU General Public License in the COPYING file at the
+    root directory of this project for more details.
+*/
+
+#ifndef __BARRY_BARRYSYNC_H__
+#define __BARRY_BARRYSYNC_H__
+
+// Include the Sync and vFormat support classes
+#include "tzwrapper.h"
+#include "vbase.h"
+#include "vcard.h"
+#include "vevent.h"
+
+#endif
+
