a2b487db426030ad7ddc48f12c83ce14c8516695
Chris Frey <cdfrey@foursquare.net>
2010-09-09 18:45:11 -0400

    maint: changed rpm build script to use spec from target branch
    Previous script used the spec file from the working directory,
    instead of the branch that was being built.

--- maintainer/release-root.sh
+++ maintainer/release-root.sh
@@ -22,14 +22,13 @@ set -e
 #
 # Build the Fedora RPM's
 #
-./make-user-rpm.sh build/barry-$1.$2.tar.bz2 ../rpm/barry.spec fedora13 f13
-./make-user-rpm.sh build/barry-$1.$2.tar.bz2 ../rpm/barry.spec fedora12 f12
-./make-user-rpm.sh build/barry-$1.$2.tar.bz2 ../rpm/barry.spec fedora11 f11
-./make-rpm.sh build/barry-$1.$2.tar.bz2 ../rpm/barry.spec fedora9 fc9
-./make-rpm.sh build/barry-$1.$2.tar.bz2 ../rpm/barry.spec fedora8 fc8
-./make-rpm.sh build/barry-$1.$2.tar.bz2 ../rpm/barry.spec fedora7 fc7
-#./make-rpm.sh build/barry-$1.$2.tar.bz2 ../rpm/barry.spec fedora6 fc6
-#./make-rpm.sh build/barry-$1.$2.tar.bz2 ../rpm/barry.spec fedora5 fc5
+THESPEC=build/barry-$1.$2/rpm/barry.spec
+./make-user-rpm.sh build/barry-$1.$2.tar.bz2 $THESPEC fedora13 f13
+./make-user-rpm.sh build/barry-$1.$2.tar.bz2 $THESPEC fedora12 f12
+./make-user-rpm.sh build/barry-$1.$2.tar.bz2 $THESPEC fedora11 f11
+./make-rpm.sh build/barry-$1.$2.tar.bz2 $THESPEC fedora9 fc9
+./make-rpm.sh build/barry-$1.$2.tar.bz2 $THESPEC fedora8 fc8
+./make-rpm.sh build/barry-$1.$2.tar.bz2 $THESPEC fedora7 fc7
 
 #
 # Build the OpenSUSE RPM's
