77a37b934dd7c69105782d25a21753896845f7a8
Chris Frey <cdfrey@foursquare.net>
2010-10-13 22:44:45 -0400

    Added missing header for select() in brawchannel.cc on Windows

--- tools/brawchannel.cc
+++ tools/brawchannel.cc
@@ -33,8 +33,9 @@
 #include <getopt.h>
 #include <fstream>
 #include <string.h>
-#include <unistd.h>
 #include <sys/types.h>
+#include <sys/time.h>
+#include <unistd.h>
 #include <signal.h>
 #include <errno.h>
 #include <pthread.h>
