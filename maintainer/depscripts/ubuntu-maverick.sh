60779380378ed0efe2cd27f49c17a534dbdf7651
Chris Frey <cdfrey@foursquare.net>
2010-11-16 22:55:54 -0500

    Added dependency "bootstrap" script for Ubuntu Maverick

---
+++ maintainer/depscripts/ubuntu-maverick.sh
@@ -0,0 +1,22 @@
+apt-get install \
+	git \
+	pkg-config \
+	fakeroot \
+	cdbs \
+	autoconf \
+	automake \
+	libtool \
+	libusb-dev \
+	libboost-serialization-dev \
+	libtar-dev \
+	libgtkmm-2.4-dev \
+	libglibmm-2.4-dev \
+	libglademm-2.4-dev \
+	libfuse-dev \
+	zlib1g-dev \
+	gettext \
+	ccache \
+	doxygen \
+	autopoint \
+	php5-cli
+
