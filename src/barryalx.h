cf92b93141149b6de41e435b30402f9ad547ec96
Chris Frey <cdfrey@foursquare.net>
2010-12-07 22:02:39 -0500

    lib: added barryalx.h "mass header" for the ALX library

---
+++ src/barryalx.h
@@ -0,0 +1,35 @@
+///
+/// \file	barryalx.h
+///		Main header file for libbarryalx
+///
+
+/*
+    Copyright (C) 2010, Net Direct Inc. (http://www.netdirect.ca/)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    See the GNU General Public License in the COPYING file at the
+    root directory of this project for more details.
+*/
+
+#ifndef __BARRY_BARRYALX_H__
+#define __BARRY_BARRYALX_H__
+
+// Include only the public backup / restore headers
+// (not tarfile)
+#include "a_common.h"
+#include "a_codsection.h"
+#include "a_library.h"
+#include "a_application.h"
+#include "a_osloader.h"
+#include "a_alxparser.h"
+
+#endif
+
