738d7660d51f9dc28ee75d8513bbbc35f59c5247
Chris Frey <cdfrey@foursquare.net>
2011-02-10 22:02:54 -0500

    scripts: disable rpath in os-4x plugin build

--- configure-barryopensync-0.4x.sh
+++ configure-barryopensync-0.4x.sh
@@ -10,5 +10,6 @@ export CXXFLAGS="-Wall -Werror -O0 -g"
 export PKG_CONFIG_PATH=/home/cdfrey/Contract/netdirect/syncberry/cvs/rootdir/lib/pkgconfig:/home/cdfrey/software/opensync/git/rootdir/lib/pkgconfig
 
 ./configure \
-	--prefix=/home/cdfrey/Contract/netdirect/syncberry/cvs/rootdir
+	--prefix=/home/cdfrey/Contract/netdirect/syncberry/cvs/rootdir \
+	--disable-rpath
 
