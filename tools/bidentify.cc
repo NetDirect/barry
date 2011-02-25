ed344d6ecf0812f4e1293cbf351e81447be67279
Chris Frey <cdfrey@foursquare.net>
2011-02-25 00:21:38 -0500

    tools: added MEID / ESN / IMEI display feature to the bidentify command

--- tools/bidentify.cc
+++ tools/bidentify.cc
@@ -41,9 +41,33 @@ void Usage()
    << "   -B bus    Specify which USB bus to search on\n"
    << "   -N dev    Specify which system device, using system specific string\n"
    << "\n"
+   << "   -c        If used with -m, show both hex and dec where possible\n"
+   << "   -m        Also show the device's ESN / MEID / IMEI\n"
    << "   -h        This help\n"
    << "   -v        Dump protocol data during operation\n"
    << endl;
+}
+
+std::string FetchMEID(Barry::Mode::Desktop &desktop)
+{
+	using namespace Barry;
+
+	unsigned int dbId = desktop.GetDBID(HandheldAgent::GetDBName());
+	RecordStateTable state;
+	desktop.GetRecordStateTable(dbId, state);
+
+	unsigned int recId = HandheldAgent::GetMEIDRecordId();
+
+	RecordStateTable::IndexType meid_index;
+	if( state.GetIndex(recId, &meid_index) ) {
+		NullStore<HandheldAgent> store;
+		RecordParser<HandheldAgent, NullStore<HandheldAgent> > parser(store);
+		desktop.GetRecord(dbId, meid_index, parser);
+		return parser.GetRecord().MEID;
+	}
+	else {
+		return string();
+	}
 }
 
 int main(int argc, char *argv[])
@@ -55,13 +79,15 @@ int main(int argc, char *argv[])
 
 	try {
 
-		bool data_dump = false;
+		bool data_dump = false,
+			get_meid = false,
+			show_both = false;
 		string busname;
 		string devname;
 
 		// process command line options
 		for(;;) {
-			int cmd = getopt(argc, argv, "B:hN:v");
+			int cmd = getopt(argc, argv, "B:hN:vmc");
 			if( cmd == -1 )
 				break;
 
@@ -73,6 +99,14 @@ int main(int argc, char *argv[])
 
 			case 'N':	// Devname
 				devname = optarg;
+				break;
+
+			case 'c':	// show both hex and dec
+				show_both = true;
+				break;
+
+			case 'm':	// get meid / esn
+				get_meid = true;
 				break;
 
 			case 'v':	// data dump on
@@ -101,7 +135,37 @@ int main(int argc, char *argv[])
 		for( int i = 0; i < probe.GetCount(); i++ ) {
 			const ProbeResult &pr = probe.Get(i);
 			cout 	<< pr.m_pin.Str() << ", "
-				<< pr.m_description << endl;
+				<< pr.m_description;
+
+			if( get_meid ) try {
+				Barry::Controller con(pr);
+				Barry::Mode::Desktop desktop(con);
+				desktop.Open();
+
+				// store as temporary in case of exceptions
+				string meid = FetchMEID(desktop);
+				cout << ", " << meid;
+
+				if( show_both ) {
+					if( HandheldAgent::IsESNHex(meid) ) {
+						cout << " (" << HandheldAgent::ESNHex2Dec(meid) << ")";
+					}
+					else if( HandheldAgent::IsESNDec(meid) ) {
+						cout << " (" << HandheldAgent::ESNDec2Hex(meid) << ")";
+					}
+				}
+			}
+			catch( Barry::BadPassword & ) {
+				// ignore password protected devices
+			}
+			catch( Barry::Error &e ) {
+				// output on cerr so as not to mess up
+				// the identify output
+				cerr << "Error on PIN: " << pr.m_pin.Str() << ": " << e.what() << endl;
+			}
+
+			// finish the identity line
+			cout << endl;
 		}
 
 		return probe.GetFailCount();
