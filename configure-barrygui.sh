a71f52c7dfa619a8d589fccb01c19216c7f7bb1f
Chris Frey <cdfrey@foursquare.net>
2011-03-14 18:19:41 -0400

    Remove -pedantic from gui compile, due to gettext.h

--- configure-barrygui.sh
+++ configure-barrygui.sh
@@ -4,7 +4,8 @@ export CC="ccache gcc"
 export CXX="ccache g++"
 
 #export CXXFLAGS="-Wall -Werror -pedantic -O0 -g -pg"
-export CXXFLAGS="-Wall -Werror -pedantic -O0 -g"
+#export CXXFLAGS="-Wall -Werror -pedantic -O0 -g"
+export CXXFLAGS="-Wall -Werror -O0 -g"
 #export CXX="g++-3.3"
 export PKG_CONFIG_PATH=/home/cdfrey/Contract/netdirect/syncberry/cvs/rootdir/lib/pkgconfig
 
