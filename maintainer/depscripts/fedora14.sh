77167bd53f4a9e216329fb308edde28e91f5b2bb
Chris Frey <cdfrey@foursquare.net>
2011-03-15 01:27:39 -0400

    maint: added dependency install script for Fedora 14

---
+++ maintainer/depscripts/fedora14.sh
@@ -0,0 +1,27 @@
+yum install \
+	git \
+	pkgconfig \
+	rpmdevtools \
+	rpm-build \
+	autoconf \
+	automake \
+	libtool \
+	gcc-c++ \
+	make \
+	libusb-devel \
+	boost-devel \
+	boost-serialization \
+	libtar-devel \
+	gtkmm24-devel \
+	glibmm24-devel \
+	libglademm24-devel \
+	fuse-devel \
+	libxml++-devel \
+	zlib-devel \
+	gettext-devel \
+	ccache \
+	doxygen \
+	wxGTK-devel \
+	libgcal-devel \
+	libopensync-devel
+
