ccdfc781f2aefadc4f18cb5cb9bf818480d8eea6
Chris Frey <cdfrey@foursquare.net>
2011-06-28 00:16:32 -0400

    test: added nightly build test script

---
+++ test/nightly.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+cd $(dirname "$0")/../maintainer
+./release.sh 0 18 0 master barrychroots
+./release.sh 0 18 0 master barryremote
+
+cd ../test
+git clean -xdf
+./buildtest.sh
+
+echo
+echo "Success."
+echo "Everything built without failure."
+
