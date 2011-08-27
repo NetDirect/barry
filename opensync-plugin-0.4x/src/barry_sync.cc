406da05e5c43c51726227dd3f3b1243187fcb3fc
Luiz Angelo Daros de Luca <luizluca@gmail.com>
2011-08-27 17:34:51 -0300

    Renaming osyncplugin set_callback functions

--- opensync-plugin-0.4x/src/barry_sync.cc
+++ opensync-plugin-0.4x/src/barry_sync.cc
@@ -1108,9 +1108,9 @@ osync_bool get_sync_info(OSyncPluginEnv *env, OSyncError **error)
 	osync_plugin_set_description(plugin, "Plugin to synchronize note, task, calendar and contact entries on USB Blackberry handhelds");
 
 	// Set the callback functions
-	osync_plugin_set_initialize(plugin, initialize);
-	osync_plugin_set_finalize(plugin, finalize);
-	osync_plugin_set_discover(plugin, discover);
+	osync_plugin_set_initialize_func(plugin, initialize);
+	osync_plugin_set_finalize_func(plugin, finalize);
+	osync_plugin_set_discover_func(plugin, discover);
 	osync_plugin_set_start_type(plugin, OSYNC_START_TYPE_PROCESS);
 
 	if( !osync_plugin_env_register_plugin(env, plugin, error) ) {
