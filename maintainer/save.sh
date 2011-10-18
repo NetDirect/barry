868fd940aba19f79f20ad4d751fd64e9d6873559
Chris Frey <cdfrey@foursquare.net>
2011-10-18 19:43:10 -0400

    maint: changed save.sh to accept empty string argument to skip pkg file rename

--- maintainer/save.sh
+++ maintainer/save.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if [ -z "$1" -o -z "$2" -o -z "$3" -o \( "$(whoami)" = "root" -a -z "$CHOWNUSER" \) ] ; then
+if [ -z "$1" -o -z "$2" -o \( "$(whoami)" = "root" -a -z "$CHOWNUSER" \) ] ; then
 	echo
 	echo "Usage: ./save.sh results_dir dir_tag file_tag [script args...]"
 	echo
 	echo "Runs the given script, and then copies all resulting binaries"
 	echo "from <results_dir>/results/ into build/<TAG> and renames all"
 	echo "the files to include <file_tag> in the name."
+	echo
+	echo "file_tag can be an empty string to skip the rename."
 	echo
 	echo "Expects CHOWNUSER to be set appropriately"
 	echo "in the environment if run as root."
@@ -36,7 +38,8 @@ fi
 
 # We do this manually in a for loop, since the rename command is
 # not the same across various linux distros...
-(
+if [ -n "$SHORTTAG" ] ; then
+	(
 	cd "build/$TAG"
 
 	for f in *.deb ; do
@@ -50,5 +53,6 @@ fi
 			mv "$f" "$(echo $f | sed "s/\.\([^.]*\)\.rpm$/.${SHORTTAG}.\1.rpm/")"
 		fi
 	done
-)
+	)
+fi
 
