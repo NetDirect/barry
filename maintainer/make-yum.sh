0d148ad2faea951602650d4a696342916fe4970c
Chris Frey <cdfrey@foursquare.net>
2011-10-19 02:12:37 -0400

    maint: added make-yum.sh for creating yum repositories under build/
    Also added yum/key file, the public key for the repositories, which
    the script adds to each repo.

---
+++ maintainer/make-yum.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#
+# Note that to use this script, you need to add "%_gpg_name" to your
+# ~/.rpmmacros file.
+#
+
+set -e
+
+# Sign all RPMs found under build/
+rpmsign --addsign $(find build -name "*.rpm" -print)
+
+# Create the YUM repo files for each architecture
+for arch in i386 source-i386 x86_64 source-x86_64 ; do
+	for dir in $(find build -name "$arch" -type d -print) ; do
+		createrepo $dir
+		cp yum/key $dir/RPM-GPG-KEY-binary-meta
+	done
+done
+
