a8fd1682f3d79cf1d3c9b0c3d60f90e63509576e
Chris Frey <cdfrey@foursquare.net>
2011-10-19 22:19:35 -0400

    maint: only prompt for gpg-agent in make-apt if environment not set

--- maintainer/make-apt.sh
+++ maintainer/make-apt.sh
@@ -2,13 +2,16 @@
 
 set -e
 
-echo "Press enter to continue if gpg-agent is running,"
-echo "Otherwise, run:"
-echo
-echo "      eval \$(gpg-agent --daemon)"
-echo
-echo "to preserve your sanity."
-read
+if [ -z "$GPG_AGENT_INFO" ] ; then
+	echo "Press enter to continue if gpg-agent is running,"
+	echo "Otherwise, run:"
+	echo
+	echo "      eval \$(gpg-agent --daemon)"
+	echo
+	echo "to preserve your sanity."
+	read
+fi
+
 
 # Build Packages and Contents, for all distros, per arch
 for arch in i386 amd64 ; do
