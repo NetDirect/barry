519e8d93869cf12ba3e5cce5aa72055073d212f7
Chris Frey <cdfrey@foursquare.net>
2011-10-19 22:22:20 -0400

    maint: added make-redirect.sh script to generate htaccess redirects

---
+++ maintainer/make-redirect.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$1" -o -z "$2" ] ; then
+	echo "make-redirect.sh prefix_dir new_url_base"
+	echo
+	echo "Creates .htaccess output for redirecting all .deb and .rpm files"
+	echo "to another website."
+	echo
+	echo "prefix_dir is the path from the redirecting site, from the"
+	echo "root of the domain name.  Must start with a slash."
+	echo "For example: /binary-meta"
+	echo
+	echo "new_url_base is the base URL of the site where the real files"
+	echo "exist.  Do not use a trailing slash, as it will be appended"
+	echo "with the filename."
+	echo
+	echo "Example:"
+	echo
+	echo "   make-redirect.sh /binary-meta http://downloads.sourceforge.net/project/barry/binary-meta/20111020"
+	echo
+	exit 1
+fi
+
+PREFIX_DIR="$1"
+NEW_URL_BASE="$2"
+
+# Build Packages and Contents, for all distros, per arch
+for file in $(cd build && find * -type f \( -name "*.rpm" -o -name "*.deb" \) -print) ; do
+	echo "Redirect 302 $PREFIX_DIR/$file $NEW_URL_BASE/$file"
+done
+
