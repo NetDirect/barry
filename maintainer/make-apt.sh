1ba127ce0a9174a7ce4cf04d641f64bbf9074d7a
Chris Frey <cdfrey@foursquare.net>
2011-10-19 22:21:06 -0400

    maint: for bm builds, put all distro targets under the dists/ directory

--- maintainer/make-apt.sh
+++ maintainer/make-apt.sh
@@ -12,25 +12,28 @@ if [ -z "$GPG_AGENT_INFO" ] ; then
 	read
 fi
 
+# Work inside the build directory, so it doesn't show up in Packages pathnames
+cd build
 
 # Build Packages and Contents, for all distros, per arch
 for arch in i386 amd64 ; do
-	for dir in $(find build -name "binary-$arch" -type d -print) ; do
-		(cd $dir && \
-		apt-ftparchive packages . | gzip -9c > Packages.gz && \
-		apt-ftparchive contents . | gzip -9c > ../../Contents-$arch.gz)
+	for dir in $(find dists -name "binary-$arch" -type d -print) ; do
+		apt-ftparchive packages $dir | tee $dir/Packages | gzip -9c > $dir/Packages.gz
+		apt-ftparchive contents $dir | gzip -9c > $dir/../../Contents-$arch.gz
 	done
 done
 
 # Build signed Release files for all debian distros
 # Make sure you have gpg-agent running, or this will be a pain...
-cd build
+cd dists
 for dir in * ; do
-	if [ -f ../apt/$dir.conf ] ; then
-		(cd $dir && apt-ftparchive -c ../../apt/$dir.conf release . > Release)
+	if [ -f ../../apt/$dir.conf ] ; then
+		(cd $dir && apt-ftparchive -c ../../../apt/$dir.conf release . > Release)
 		gpg --use-agent --default-key B6C2250E --armor \
 			--output $dir/Release.gpg \
 			--detach-sign $dir/Release
+	else
+		echo "WARNING: conf file for $dir not found under apt/"
 	fi
 done
 
