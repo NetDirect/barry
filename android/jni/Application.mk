49f886f8b72af8393a9ac6c6a7d365b7becdacca
Toby Gray <toby.gray@realvnc.com>
2011-10-31 16:02:00 +0000

    build: Only building armeabi on Android.

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,3 +1,4 @@
-APP_ABI := armeabi armeabi-v7a
+#Can also set this to other values such as APP_ABI := armeabi armeabi-v7a x86
+APP_ABI := armeabi
 APP_STL := gnustl_static
 APP_CPPFLAGS += -fexceptions
