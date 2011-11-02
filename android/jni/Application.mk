6ec80298b2858e11cd3739c20aaf56b6f113a856
Toby Gray <toby.gray@realvnc.com>
2011-11-02 15:19:53 +0000

    build: Adding Android Makefile to automake to allow it to appear in dist tarballs.

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,4 +1,4 @@
 #Can also set this to other values such as APP_ABI := armeabi armeabi-v7a x86
 APP_ABI := armeabi
 APP_STL := gnustl_static
-APP_CPPFLAGS += -fexceptions
+APP_CPPFLAGS := -fexceptions
