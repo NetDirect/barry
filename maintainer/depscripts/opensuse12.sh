f2709136562b0ab1914a63819e639bf1537eefe4
Chris Frey <cdfrey@foursquare.net>
2011-11-21 20:20:00 -0500

    maint: added depscript for opensuse 12.1

---
+++ maintainer/depscripts/opensuse12.sh
@@ -0,0 +1,33 @@
+zypper install \
+	git \
+	pkg-config \
+	createrepo \
+	desktop-file-utils \
+	autoconf \
+	autoconf-archive \
+	automake \
+	libtool \
+	gcc-c++ \
+	make \
+	libusb-1_0-devel \
+	boost-devel \
+	libtar-devel \
+	gtkmm2-devel \
+	glibmm2-devel \
+	libglademm-devel \
+	fuse-devel \
+	libxml++-devel \
+	zlib-devel \
+	gettext-tools \
+	ccache \
+	doxygen \
+	wxWidgets-devel \
+	libSDL-devel \
+	libgcal-devel \
+	evolution-data-server-devel
+
+#	rpmdevtools \
+#	rpm-build \
+#	rpm-sign \
+#	libopensync-devel
+
