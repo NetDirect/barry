3f1a842cebe0dee2b657bae866bcbfe1858a894c
Chris Frey <cdfrey@foursquare.net>
2012-01-23 22:09:55 -0500

    Cleaned up the main configure script and added optional arg

--- configure-barry.sh
+++ configure-barry.sh
@@ -1,45 +1,15 @@
 #!/bin/sh
 
-CHOICE="system"
-
 #export CXXFLAGS="-Wall -Werror -pedantic -O0 -g -pg"
 export CXXFLAGS="-Wall -Werror -O0 -g"
-#export CXX="g++-3.3"
-#export CXX="g++-3.4"
-#export CXX="g++-4.1"
-#export CXX="/home/cdfrey/software/gcc/rootdir-4.3.0/bin/g++"
 
 export CC="ccache gcc"
 export CXX="ccache g++"
 
 export BOOSTOPT="--enable-boost"
 
-if [ -n "$1" ] ; then
-	CHOICE="$1"
-fi
-
-if [ "$CHOICE" = "devel" ] ; then
-echo "Configuring for: devel"
 ./configure \
 	--prefix=/home/cdfrey/Contract/netdirect/syncberry/cvs/rootdir \
-	--with-libusb=/home/cdfrey/Contract/netdirect/syncberry/cvs/external/rootdir/libusb \
-	$BOOSTOPT
-fi
-
-
-if [ "$CHOICE" = "system" ] ; then
-echo "Configuring for: system"
-./configure \
-	--prefix=/home/cdfrey/Contract/netdirect/syncberry/cvs/rootdir \
-	$BOOSTOPT
-fi
-
-
-if [ "$CHOICE" = "stable" ] ; then
-echo "Configuring for: stable"
-./configure \
-	--prefix=/home/cdfrey/Contract/netdirect/syncberry/cvs/rootdir \
-	--with-libusb=/home/cdfrey/Contract/netdirect/syncberry/cvs/external/rootdir/libusb-stable \
-	$BOOSTOPT
-fi
+	$BOOSTOPT \
+	$1
 
