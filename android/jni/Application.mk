b0fcf38593c395757354f99be7596053094cc083
Toby Gray <toby.gray@realvnc.com>
2012-03-06 10:32:05 +0000

    Android: Adding RTTI flag to allow tr1::shared_ptr to work on NDK r7b.

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,4 +1,5 @@
 #Can also set this to other values such as APP_ABI := armeabi armeabi-v7a x86
 APP_ABI := armeabi
 APP_STL := gnustl_static
-APP_CPPFLAGS := -fexceptions
+# Need rtti for shared_ptr support to work on NDK r7b
+APP_CPPFLAGS := -fexceptions -frtti
