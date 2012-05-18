5d4155755f66f0b3546c3e8d85cd8c8bbbabcc94
Chris Frey <cdfrey@foursquare.net>
2012-05-17 22:04:12 -0400

    maint: changed abi checker script to use git tags instead of tarballs

--- maintainer/abi/abi-prepare.sh
+++ maintainer/abi/abi-prepare.sh
@@ -6,11 +6,11 @@ fullpath() {
 }
 
 if [ -z "$1" -o -z "$2" -o -z "$3" ] ; then
-	echo "Usage: abi-prepare.sh tarball abi.xml configure.sh"
+	echo "Usage: abi-prepare.sh commit abi.xml configure.sh"
 	exit 1
 fi
 
-TARBALL="$1"
+COMMIT="$1"
 XML="$2"
 CONFIG="$3"
 VERSION="$(grep -A 1 '<version>' "$XML" | grep -v version | sed "s/^[ 	]//")"
@@ -25,11 +25,18 @@ set -e
 echo "Creating source tree..."
 mkdir "/tmp/$VERSION"			# make sure it doesn't already exist
 mkdir -p "/tmp/$VERSION/rootdir"
-tar -C "/tmp/$VERSION" -xjf "$TARBALL"
+
+# extract the commit... need to prefix rev-parse with ./ since if run
+# in root of git repo, rev-parse will output an empty string,
+# which we don't want to pass to cd.
+(cd "./$(git rev-parse --show-cdup)" && \
+	git archive --prefix="$COMMIT/" "$COMMIT") | \
+	tar -C "/tmp/$VERSION" -xf -
 
 # build and install
 echo "Installing to: /tmp/$VERSION/rootdir..."
-cd "/tmp/$VERSION/barry-$VERSION"
+cd "/tmp/$VERSION/$COMMIT"
+./buildgen.sh
 "$FULLCONFIG" --prefix="/tmp/$VERSION/rootdir"
 make -j2
 make install
